9615db885d017122101c319fc7649891f090c4b6
any2cards <any2cards@yahoo.com>
2018-05-14 07:44:06 -0700

    Add files via upload

---
+++ data/battle-goals.js
@@ -0,0 +1,152 @@
+[
+  {
+    "name": "aggressor",
+    "points": 0,
+    "image": "battle-goals/aggressor.png",
+    "xws": "aggressor"
+  },
+  {
+    "name": "battlegoal-back",
+    "points": 1,
+    "image": "battle-goals/battlegoal-back.png",
+    "xws": "battlegoalback"
+  },
+  {
+    "name": "diehard",
+    "points": 2,
+    "image": "battle-goals/diehard.png",
+    "xws": "diehard"
+  },
+  {
+    "name": "dynamo",
+    "points": 3,
+    "image": "battle-goals/dynamo.png",
+    "xws": "dynamo"
+  },
+  {
+    "name": "executioner",
+    "points": 4,
+    "image": "battle-goals/executioner.png",
+    "xws": "executioner"
+  },
+  {
+    "name": "explorer",
+    "points": 5,
+    "image": "battle-goals/explorer.png",
+    "xws": "explorer"
+  },
+  {
+    "name": "fast healer",
+    "points": 6,
+    "image": "battle-goals/fasthealer.png",
+    "xws": "fasthealer"
+  },
+  {
+    "name": "hoarder",
+    "points": 7,
+    "image": "battle-goals/hoarder.png",
+    "xws": "hoarder"
+  },
+  {
+    "name": "hunter",
+    "points": 8,
+    "image": "battle-goals/hunter.png",
+    "xws": "hunter"
+  },
+  {
+    "name": "indigent",
+    "points": 9,
+    "image": "battle-goals/indigent.png",
+    "xws": "indigent"
+  },
+  {
+    "name": "layabout",
+    "points": 10,
+    "image": "battle-goals/layabout.png",
+    "xws": "layabout"
+  },
+  {
+    "name": "masochist",
+    "points": 11,
+    "image": "battle-goals/masochist.png",
+    "xws": "masochist"
+  },
+  {
+    "name": "neutralizer",
+    "points": 12,
+    "image": "battle-goals/neutralizer.png",
+    "xws": "neutralizer"
+  },
+  {
+    "name": "opener",
+    "points": 13,
+    "image": "battle-goals/opener.png",
+    "xws": "opener"
+  },
+  {
+    "name": "pacifist",
+    "points": 14,
+    "image": "battle-goals/pacifist.png",
+    "xws": "pacifist"
+  },
+  {
+    "name": "plunderer",
+    "points": 15,
+    "image": "battle-goals/plunderer.png",
+    "xws": "plunderer"
+  },
+  {
+    "name": "professional",
+    "points": 16,
+    "image": "battle-goals/professional.png",
+    "xws": "professional"
+  },
+  {
+    "name": "protector",
+    "points": 17,
+    "image": "battle-goals/protector.png",
+    "xws": "protector"
+  },
+  {
+    "name": "purist",
+    "points": 18,
+    "image": "battle-goals/purist.png",
+    "xws": "purist"
+  },
+  {
+    "name": "sadist",
+    "points": 19,
+    "image": "battle-goals/sadist.png",
+    "xws": "sadist"
+  },
+  {
+    "name": "scrambler",
+    "points": 20,
+    "image": "battle-goals/scrambler.png",
+    "xws": "scrambler"
+  },
+  {
+    "name": "straggler",
+    "points": 21,
+    "image": "battle-goals/straggler.png",
+    "xws": "straggler"
+  },
+  {
+    "name": "streamliner",
+    "points": 22,
+    "image": "battle-goals/streamliner.png",
+    "xws": "streamliner"
+  },
+  {
+    "name": "workhorse",
+    "points": 23,
+    "image": "battle-goals/workhorse.png",
+    "xws": "workhorse"
+  },
+  {
+    "name": "zealot",
+    "points": 24,
+    "image": "battle-goals/zealot.png",
+    "xws": "zealot"
+  }
+]
