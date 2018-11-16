ff34b6fa44f2f33da4e19d496f07f74ae2d36a0c
any2cards <any2cards@yahoo.com>
2018-11-15 18:47:25 -0800

    Add Character Perks

---
+++ data/character-perks.js
@@ -0,0 +1,116 @@
+[
+  {
+    "name": "beast tyrant perks",
+    "points": 0,
+    "image": "character-perks/beast-tyrant-perks.png",
+    "xws": "beasttyrantperks"
+  },
+  {
+    "name": "beserker perks",
+    "points": 0,
+    "image": "character-perks/beserker-perks.png",
+    "xws": "beserkerperks"
+  },
+  {
+    "name": "bladeswarm perks",
+    "points": 0,
+    "image": "character-perks/bladeswarm-perks.png",
+    "xws": "bladeswarmperks"
+  },
+  {
+    "name": "brute perks",
+    "points": 0,
+    "image": "character-perks/brute-perks.png",
+    "xws": "bruteperks"
+  },
+  {
+    "name": "cragheart perks",
+    "points": 0,
+    "image": "character-perks/cragheart-perks.png",
+    "xws": "cragheartperks"
+  },
+  {
+    "name": "diviner perks",
+    "points": 0,
+    "image": "character-perks/diviner-perks.png",
+    "xws": "divinerperks"
+  },
+  {
+    "name": "doomstalker perks",
+    "points": 0,
+    "image": "character-perks/doomstalker-perks.png",
+    "xws": "doomstalkerperks"
+  },
+  {
+    "name": "elementalist perks",
+    "points": 0,
+    "image": "character-perks/elementalist-perks.png",
+    "xws": "elementalistperks"
+  },
+  {
+    "name": "mindthief perks",
+    "points": 0,
+    "image": "character-perks/mindthief-perks.png",
+    "xws": "mindthiefperks"
+  },
+  {
+    "name": "nightshroud perks",
+    "points": 0,
+    "image": "character-perks/nightshroud-perks.png",
+    "xws": "nightshroudperks"
+  },
+  {
+    "name": "plagueherald perks",
+    "points": 0,
+    "image": "character-perks/plagueherald-perks.png",
+    "xws": "plagueheraldperks"
+  },
+  {
+    "name": "quartermaster perks",
+    "points": 0,
+    "image": "character-perks/quartermaster-perks.png",
+    "xws": "quartermasterperks"
+  },
+  {
+    "name": "sawbones perks",
+    "points": 0,
+    "image": "character-perks/sawbones-perks.png",
+    "xws": "sawbonesperks"
+  },
+  {
+    "name": "scoundrel perks",
+    "points": 0,
+    "image": "character-perks/scoundrel-perks.png",
+    "xws": "scoundrelperks"
+  },
+  {
+    "name": "soothsinger perks",
+    "points": 0,
+    "image": "character-perks/soothsinger-perks.png",
+    "xws": "soothsingerperks"
+  },
+  {
+    "name": "spellweaver perks",
+    "points": 0,
+    "image": "character-perks/spellweaver-perks.png",
+    "xws": "spellweaverperks"
+  },
+  {
+    "name": "summoner perks",
+    "points": 0,
+    "image": "character-perks/summoner-perks.png",
+    "xws": "summonerperks"
+  },
+  {
+    "name": "sunkeeper perks",
+    "points": 0,
+    "image": "character-perks/sunkeeper-perks.png",
+    "xws": "sunkeeperperks"
+  },
+  {
+    "name": "tinkerer perks",
+    "points": 0,
+    "image": "character-perks/tinkerer-perks.png",
+    "xws": "tinkererperks"
+  }
+]
