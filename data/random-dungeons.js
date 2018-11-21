5046aa675cec8e86757b3c6c8f5396597dea7621
any2cards <any2cards@yahoo.com>
2018-11-20 16:39:40 -0800

    Add Random Dungeons & Monsters

---
+++ data/random-dungeons.js
@@ -0,0 +1,506 @@
+[
+  {
+    "name": "random dungeon a-back",
+    "points": 0,
+    "image": "random-dungeons/dungeons/dungeon-a-back.png",
+    "xws": "randomdungeonaback"
+  },
+  {
+    "name": "random dungeon ab-back",
+    "points": 1,
+    "image": "random-dungeons/dungeons/dungeon-ab-back.png",
+    "xws": "randomdungeonabback"
+  },
+  {
+    "name": "random dungeon alcove",
+    "points": 2,
+    "image": "random-dungeons/dungeons/dungeon-alcove.png",
+    "xws": "randomdungeonalcove"
+  },
+  {
+    "name": "random dungeon altar",
+    "points": 3,
+    "image": "random-dungeons/dungeons/dungeon-altar.png",
+    "xws": "randomdungeonaltar"
+  },
+  {
+    "name": "random dungeon armory",
+    "points": 4,
+    "image": "random-dungeons/dungeons/dungeon-armory.png",
+    "xws": "randomdungeonarmory"
+  },
+  {
+    "name": "random dungeon b-back",
+    "points": 5,
+    "image": "random-dungeons/dungeons/dungeon-b-back.png",
+    "xws": "randomdungeonbback"
+  },
+  {
+    "name": "random dungeon burrow",
+    "points": 6,
+    "image": "random-dungeons/dungeons/dungeon-burrow.png",
+    "xws": "randomdungeonburrow"
+  },
+  {
+    "name": "random dungeon cabin",
+    "points": 7,
+    "image": "random-dungeons/dungeons/dungeon-cabin.png",
+    "xws": "randomdungeoncabin"
+  },
+  {
+    "name": "random dungeon cave",
+    "points": 8,
+    "image": "random-dungeons/dungeons/dungeon-cave.png",
+    "xws": "randomdungeoncave"
+  },
+  {
+    "name": "random dungeon clearing",
+    "points": 9,
+    "image": "random-dungeons/dungeons/dungeon-clearing.png",
+    "xws": "randomdungeonclearing"
+  },
+  {
+    "name": "random dungeon corridor",
+    "points": 10,
+    "image": "random-dungeons/dungeons/dungeon-corridor.png",
+    "xws": "randomdungeoncorridor"
+  },
+  {
+    "name": "random dungeon crossroads",
+    "points": 11,
+    "image": "random-dungeons/dungeons/dungeon-crossroads.png",
+    "xws": "randomdungeoncrossroads"
+  },
+  {
+    "name": "random dungeon dead-end",
+    "points": 12,
+    "image": "random-dungeons/dungeons/dungeon-dead-end.png",
+    "xws": "randomdungeondeadend"
+  },
+  {
+    "name": "random dungeon den",
+    "points": 13,
+    "image": "random-dungeons/dungeons/dungeon-den.png",
+    "xws": "randomdungeonden"
+  },
+  {
+    "name": "random dungeon encampment",
+    "points": 14,
+    "image": "random-dungeons/dungeons/dungeon-encampment.png",
+    "xws": "randomdungeonencampment"
+  },
+  {
+    "name": "random dungeon hallway",
+    "points": 15,
+    "image": "random-dungeons/dungeons/dungeon-hallway.png",
+    "xws": "randomdungeonhallway"
+  },
+  {
+    "name": "random dungeon hovel",
+    "points": 16,
+    "image": "random-dungeons/dungeons/dungeon-hovel.png",
+    "xws": "randomdungeonhovel"
+  },
+  {
+    "name": "random dungeon library",
+    "points": 17,
+    "image": "random-dungeons/dungeons/dungeon-library.png",
+    "xws": "randomdungeonlibrary"
+  },
+  {
+    "name": "random dungeon passage",
+    "points": 18,
+    "image": "random-dungeons/dungeons/dungeon-passage.png",
+    "xws": "randomdungeonpassage"
+  },
+  {
+    "name": "random dungeon road",
+    "points": 19,
+    "image": "random-dungeons/dungeons/dungeon-road.png",
+    "xws": "randomdungeonroad"
+  },
+  {
+    "name": "random dungeon sewer",
+    "points": 20,
+    "image": "random-dungeons/dungeons/dungeon-sewer.png",
+    "xws": "randomdungeonsewer"
+  },
+  {
+    "name": "random dungeon trail",
+    "points": 21,
+    "image": "random-dungeons/dungeons/dungeon-trail.png",
+    "xws": "randomdungeontrail"
+  },
+  {
+    "name": "random dungeon tunnel",
+    "points": 22,
+    "image": "random-dungeons/dungeons/dungeon-tunnel.png",
+    "xws": "randomdungeontunnel"
+  },
+  {
+    "name": "random dungeon archaic",
+    "points": 23,
+    "image": "random-dungeons/monsters/monster-archaic.png",
+    "xws": "randomdungeonarchaic"
+  },
+  {
+    "name": "random dungeon corrupted",
+    "points": 24,
+    "image": "random-dungeons/monsters/monster-corrupted.png",
+    "xws": "randomdungeoncorrupted"
+  },
+  {
+    "name": "random dungeon crushing",
+    "points": 25,
+    "image": "random-dungeons/monsters/monster-crushing.png",
+    "xws": "randomdungeoncrushing"
+  },
+  {
+    "name": "random dungeon cutthroat",
+    "points": 26,
+    "image": "random-dungeons/monsters/monster-cutthroat.png",
+    "xws": "randomdungeoncutthroat"
+  },
+  {
+    "name": "random dungeon defiled",
+    "points": 27,
+    "image": "random-dungeons/monsters/monster-defiled.png",
+    "xws": "randomdungeondefiled"
+  },
+  {
+    "name": "random dungeon drowned",
+    "points": 28,
+    "image": "random-dungeons/monsters/monster-drowned.png",
+    "xws": "randomdungeondrowned"
+  },
+  {
+    "name": "random dungeon foggy",
+    "points": 29,
+    "image": "random-dungeons/monsters/monster-foggy.png",
+    "xws": "randomdungeonfoggy"
+  },
+  {
+    "name": "random dungeon fortified",
+    "points": 30,
+    "image": "random-dungeons/monsters/monster-fortified.png",
+    "xws": "randomdungeonfortified"
+  },
+  {
+    "name": "random dungeon frigid",
+    "points": 31,
+    "image": "random-dungeons/monsters/monster-frigid.png",
+    "xws": "randomdungeonfrigid"
+  },
+  {
+    "name": "random dungeon hopeless",
+    "points": 32,
+    "image": "random-dungeons/monsters/monster-hopeless.png",
+    "xws": "randomdungeonhopeless"
+  },
+  {
+    "name": "random dungeon horrific",
+    "points": 33,
+    "image": "random-dungeons/monsters/monster-horrific.png",
+    "xws": "randomdungeonhorrific"
+  },
+  {
+    "name": "random dungeon infected",
+    "points": 34,
+    "image": "random-dungeons/monsters/monster-infected.png",
+    "xws": "randomdungeoninfected"
+  },
+  {
+    "name": "random dungeon mangy",
+    "points": 35,
+    "image": "random-dungeons/monsters/monster-mangy.png",
+    "xws": "randomdungeonmangy"
+  },
+  {
+    "name": "random dungeon monster-back",
+    "points": 36,
+    "image": "random-dungeons/monsters/monster-monster-back.png",
+    "xws": "randomdungeonmonsterback"
+  },
+  {
+    "name": "random dungeon putrid",
+    "points": 37,
+    "image": "random-dungeons/monsters/monster-putrid.png",
+    "xws": "randomdungeonputrid"
+  },
+  {
+    "name": "random dungeon rotting",
+    "points": 38,
+    "image": "random-dungeons/monsters/monster-rotting.png",
+    "xws": "randomdungeonrotting"
+  },
+  {
+    "name": "random dungeon scaled",
+    "points": 39,
+    "image": "random-dungeons/monsters/monster-scaled.png",
+    "xws": "randomdungeonscaled"
+  },
+  {
+    "name": "random dungeon tribal",
+    "points": 40,
+    "image": "random-dungeons/monsters/monster-tribal.png",
+    "xws": "randomdungeontribal"
+  },
+  {
+    "name": "random dungeon unstable",
+    "points": 41,
+    "image": "random-dungeons/monsters/monster-unstable.png",
+    "xws": "randomdungeonunstable"
+  },
+  {
+    "name": "random dungeon venomous",
+    "points": 42,
+    "image": "random-dungeons/monsters/monster-venomous.png",
+    "xws": "randomdungeonvenomous"
+  },
+  {
+    "name": "random dungeon wild",
+    "points": 43,
+    "image": "random-dungeons/monsters/monster-wild.png",
+    "xws": "randomdungeonwild"
+  },
+  {
+    "name": "random dungeon 551",
+    "points": 44,
+    "image": "random-dungeons/dungeons/dungeon-alcove.png",
+    "xws": "randomdungeonalcove"
+  },
+  {
+    "name": "random dungeon 542",
+    "points": 45,
+    "image": "random-dungeons/dungeons/dungeon-altar.png",
+    "xws": "randomdungeonaltar"
+  },
+  {
+    "name": "random dungeon 543",
+    "points": 46,
+    "image": "random-dungeons/dungeons/dungeon-armory.png",
+    "xws": "randomdungeonarmory"
+  },
+  {
+    "name": "random dungeon 553",
+    "points": 47,
+    "image": "random-dungeons/dungeons/dungeon-burrow.png",
+    "xws": "randomdungeonburrow"
+  },
+  {
+    "name": "random dungeon 540",
+    "points": 48,
+    "image": "random-dungeons/dungeons/dungeon-cabin.png",
+    "xws": "randomdungeoncabin"
+  },
+  {
+    "name": "random dungeon 549",
+    "points": 49,
+    "image": "random-dungeons/dungeons/dungeon-cave.png",
+    "xws": "randomdungeoncave"
+  },
+  {
+    "name": "random dungeon 541",
+    "points": 50,
+    "image": "random-dungeons/dungeons/dungeon-clearing.png",
+    "xws": "randomdungeonclearing"
+  },
+  {
+    "name": "random dungeon 548",
+    "points": 51,
+    "image": "random-dungeons/dungeons/dungeon-corridor.png",
+    "xws": "randomdungeoncorridor"
+  },
+  {
+    "name": "random dungeon 535",
+    "points": 52,
+    "image": "random-dungeons/dungeons/dungeon-crossroads.png",
+    "xws": "randomdungeoncrossroads"
+  },
+  {
+    "name": "random dungeon 537",
+    "points": 53,
+    "image": "random-dungeons/dungeons/dungeon-dead-end.png",
+    "xws": "randomdungeondeadend"
+  },
+  {
+    "name": "random dungeon 546",
+    "points": 54,
+    "image": "random-dungeons/dungeons/dungeon-den.png",
+    "xws": "randomdungeonden"
+  },
+  {
+    "name": "random dungeon 544",
+    "points": 55,
+    "image": "random-dungeons/dungeons/dungeon-encampment.png",
+    "xws": "randomdungeonencampment"
+  },
+  {
+    "name": "random dungeon 538",
+    "points": 56,
+    "image": "random-dungeons/dungeons/dungeon-hallway.png",
+    "xws": "randomdungeonhallway"
+  },
+  {
+    "name": "random dungeon 552",
+    "points": 57,
+    "image": "random-dungeons/dungeons/dungeon-hovel.png",
+    "xws": "randomdungeonhovel"
+  },
+  {
+    "name": "random dungeon 545",
+    "points": 58,
+    "image": "random-dungeons/dungeons/dungeon-library.png",
+    "xws": "randomdungeonlibrary"
+  },
+  {
+    "name": "random dungeon 536",
+    "points": 59,
+    "image": "random-dungeons/dungeons/dungeon-passage.png",
+    "xws": "randomdungeonpassage"
+  },
+  {
+    "name": "random dungeon 539",
+    "points": 60,
+    "image": "random-dungeons/dungeons/dungeon-road.png",
+    "xws": "randomdungeonroad"
+  },
+  {
+    "name": "random dungeon 534",
+    "points": 61,
+    "image": "random-dungeons/dungeons/dungeon-sewer.png",
+    "xws": "randomdungeonsewer"
+  },
+  {
+    "name": "random dungeon 547",
+    "points": 62,
+    "image": "random-dungeons/dungeons/dungeon-trail.png",
+    "xws": "randomdungeontrail"
+  },
+  {
+    "name": "random dungeon 550",
+    "points": 63,
+    "image": "random-dungeons/dungeons/dungeon-tunnel.png",
+    "xws": "randomdungeontunnel"
+  },
+  {
+    "name": "random dungeon 566",
+    "points": 64,
+    "image": "random-dungeons/monsters/monster-archaic.png",
+    "xws": "randomdungeonarchaic"
+  },
+  {
+    "name": "random dungeon 570",
+    "points": 65,
+    "image": "random-dungeons/monsters/monster-corrupted.png",
+    "xws": "randomdungeoncorrupted"
+  },
+  {
+    "name": "random dungeon 563",
+    "points": 66,
+    "image": "random-dungeons/monsters/monster-crushing.png",
+    "xws": "randomdungeoncrushing"
+  },
+  {
+    "name": "random dungeon 560",
+    "points": 67,
+    "image": "random-dungeons/monsters/monster-cutthroat.png",
+    "xws": "randomdungeoncutthroat"
+  },
+  {
+    "name": "random dungeon 557",
+    "points": 68,
+    "image": "random-dungeons/monsters/monster-defiled.png",
+    "xws": "randomdungeondefiled"
+  },
+  {
+    "name": "random dungeon 555",
+    "points": 69,
+    "image": "random-dungeons/monsters/monster-drowned.png",
+    "xws": "randomdungeondrowned"
+  },
+  {
+    "name": "random dungeon 562",
+    "points": 70,
+    "image": "random-dungeons/monsters/monster-foggy.png",
+    "xws": "randomdungeonfoggy"
+  },
+  {
+    "name": "random dungeon 559",
+    "points": 71,
+    "image": "random-dungeons/monsters/monster-fortified.png",
+    "xws": "randomdungeonfortified"
+  },
+  {
+    "name": "random dungeon 573",
+    "points": 72,
+    "image": "random-dungeons/monsters/monster-frigid.png",
+    "xws": "randomdungeonfrigid"
+  },
+  {
+    "name": "random dungeon 556",
+    "points": 73,
+    "image": "random-dungeons/monsters/monster-hopeless.png",
+    "xws": "randomdungeonhopeless"
+  },
+  {
+    "name": "random dungeon 565",
+    "points": 74,
+    "image": "random-dungeons/monsters/monster-horrific.png",
+    "xws": "randomdungeonhorrific"
+  },
+  {
+    "name": "random dungeon 571",
+    "points": 75,
+    "image": "random-dungeons/monsters/monster-infected.png",
+    "xws": "randomdungeoninfected"
+  },
+  {
+    "name": "random dungeon 554",
+    "points": 76,
+    "image": "random-dungeons/monsters/monster-mangy.png",
+    "xws": "randomdungeonmangy"
+  },
+  {
+    "name": "random dungeon 572",
+    "points": 77,
+    "image": "random-dungeons/monsters/monster-putrid.png",
+    "xws": "randomdungeonputrid"
+  },
+  {
+    "name": "random dungeon 561",
+    "points": 78,
+    "image": "random-dungeons/monsters/monster-rotting.png",
+    "xws": "randomdungeonrotting"
+  },
+  {
+    "name": "random dungeon 567",
+    "points": 79,
+    "image": "random-dungeons/monsters/monster-scaled.png",
+    "xws": "randomdungeonscaled"
+  },
+  {
+    "name": "random dungeon 564",
+    "points": 80,
+    "image": "random-dungeons/monsters/monster-tribal.png",
+    "xws": "randomdungeontribal"
+  },
+  {
+    "name": "random dungeon 569",
+    "points": 81,
+    "image": "random-dungeons/monsters/monster-unstable.png",
+    "xws": "randomdungeonunstable"
+  },
+  {
+    "name": "random dungeon 568",
+    "points": 82,
+    "image": "random-dungeons/monsters/monster-venomous.png",
+    "xws": "randomdungeonvenomous"
+  },
+  {
+    "name": "random dungeon 558",
+    "points": 83,
+    "image": "random-dungeons/monsters/monster-wild.png",
+    "xws": "randomdungeonwild"
+  }
+]
