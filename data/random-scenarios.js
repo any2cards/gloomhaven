5f23ec6a5b5be85bce06513a7998aa91f491b11e
any2cards <any2cards@yahoo.com>
2018-11-22 07:46:32 -0800

    Add Random Scenarios

---
+++ data/random-scenarios.js
@@ -0,0 +1,116 @@
+[
+  {
+    "name": "arcane library",
+    "points": 0,
+    "image": "random-scenarios/arcane-library.png",
+    "xws": "arcanelibrary"
+  },
+  {
+    "name": "chained isle",
+    "points": 1,
+    "image": "random-scenarios/chained-isle.png",
+    "xws": "chainedisle"
+  },
+  {
+    "name": "clockwork cove",
+    "points": 2,
+    "image": "random-scenarios/clockwork cove.png",
+    "xws": "clockworkcove"
+  },
+  {
+    "name": "magma pit",
+    "points": 3,
+    "image": "random-scenarios/magma-pit.png",
+    "xws": "magmapit"
+  },
+  {
+    "name": "random scenario back",
+    "points": 4,
+    "image": "random-scenarios/random-scenario-back.png",
+    "xws": "randomscenarioback"
+  },
+  {
+    "name": "sulfer mine",
+    "points": 5,
+    "image": "random-scenarios/sulfer-mine.png",
+    "xws": "sulfermine"
+  },
+  {
+    "name": "toxic moor",
+    "points": 6,
+    "image": "random-scenarios/toxic-moor.png",
+    "xws": "toxicmoor"
+  },
+  {
+    "name": "underwater lagoon",
+    "points": 7,
+    "image": "random-scenarios/underwater-lagoon.png",
+    "xws": "underwaterlagoon"
+  },
+  {
+    "name": "well of the unfortunate",
+    "points": 8,
+    "image": "random-scenarios/well-of-the-unfortunate.png",
+    "xws": "welloftheunfortunate"
+  },
+  {
+    "name": "windswept highlands",
+    "points": 9,
+    "image": "random-scenarios/windswept-highlands.png",
+    "xws": "windswepthighlands"
+  },
+  {
+    "name": "random scenario 67",
+    "points": 10,
+    "image": "random-scenarios/arcane-library.png",
+    "xws": "arcanelibrary"
+  },
+  {
+    "name": "random scenario 70",
+    "points": 11,
+    "image": "random-scenarios/chained-isle.png",
+    "xws": "chainedisle"
+  },
+  {
+    "name": "random scenario 66",
+    "points": 12,
+    "image": "random-scenarios/clockwork cove.png",
+    "xws": "clockworkcove"
+  },
+  {
+    "name": "random scenario 63",
+    "points": 13,
+    "image": "random-scenarios/magma-pit.png",
+    "xws": "magmapit"
+  },
+  {
+    "name": "random scenario 65",
+    "points": 14,
+    "image": "random-scenarios/sulfer-mine.png",
+    "xws": "sulfermine"
+  },
+  {
+    "name": "random scenario 68",
+    "points": 15,
+    "image": "random-scenarios/toxic-moor.png",
+    "xws": "toxicmoor"
+  },
+  {
+    "name": "random scenario 64",
+    "points": 16,
+    "image": "random-scenarios/underwater-lagoon.png",
+    "xws": "underwaterlagoon"
+  },
+  {
+    "name": "random scenario 69",
+    "points": 17,
+    "image": "random-scenarios/well-of-the-unfortunate.png",
+    "xws": "welloftheunfortunate"
+  },
+  {
+    "name": "random scenario 71",
+    "points": 18,
+    "image": "random-scenarios/windswept-highlands.png",
+    "xws": "windswepthighlands"
+  }
+]
