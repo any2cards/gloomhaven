8613aafccadef4b78ff4ad9cdb2e854d7c7f2b05
any2cards <any2cards@yahoo.com>
2018-11-22 12:56:01 -0800

    Add Overlay Tokens

---
+++ data/overlay-tokens.js
@@ -0,0 +1,290 @@
+[
+  {
+    "name": "earth",
+    "points": 0,
+    "image": "overlay-tokens/corridors/earth-1.png",
+    "xws": "earth"
+  },
+  {
+    "name": "earth",
+    "points": 1,
+    "image": "overlay-tokens/corridors/earth-2.png",
+    "xws": "earth"
+  },
+  {
+    "name": "stone",
+    "points": 2,
+    "image": "overlay-tokens/corridors/man-made-stone-1.png",
+    "xws": "manmadestone"
+  },
+  {
+    "name": "stone",
+    "points": 3,
+    "image": "overlay-tokens/corridors/man-made-stone-2.png",
+    "xws": "manmadestone"
+  },
+  {
+    "name": "stone",
+    "points": 4,
+    "image": "overlay-tokens/corridors/natural-stone-1.png",
+    "xws": "naturalstone"
+  },
+  {
+    "name": "stone",
+    "points": 5,
+    "image": "overlay-tokens/corridors/natural-stone-2.png",
+    "xws": "naturalstone"
+  },
+  {
+    "name": "pressure plate",
+    "points": 6,
+    "image": "overlay-tokens/corridors/pressure-plate.png",
+    "xws": "pressureplate"
+  },
+  {
+    "name": "wood",
+    "points": 7,
+    "image": "overlay-tokens/corridors/wood-1.png",
+    "xws": "wood"
+  },
+  {
+    "name": "wood",
+    "points": 8,
+    "image": "overlay-tokens/corridors/wood-2.png",
+    "xws": "wood"
+  },
+  {
+    "name": "log",
+    "points": 9,
+    "image": "overlay-tokens/difficult-terrain/log.png",
+    "xws": "log"
+  },
+  {
+    "name": "rubble",
+    "points": 10,
+    "image": "overlay-tokens/difficult-terrain/rubble.png",
+    "xws": "rubble"
+  },
+  {
+    "name": "stairs",
+    "points": 11,
+    "image": "overlay-tokens/difficult-terrain/stairs.png",
+    "xws": "stairs"
+  },
+  {
+    "name": "water",
+    "points": 12,
+    "image": "overlay-tokens/difficult-terrain/water.png",
+    "xws": "water"
+  },
+  {
+    "name": "door",
+    "points": 13,
+    "image": "overlay-tokens/doors/dark-fog.png",
+    "xws": "darkfog"
+  },
+  {
+    "name": "door",
+    "points": 14,
+    "image": "overlay-tokens/doors/light-fog.png",
+    "xws": "lightfog"
+  },
+  {
+    "name": "door",
+    "points": 15,
+    "image": "overlay-tokens/doors/stone-door.png",
+    "xws": "stonedoor"
+  },
+  {
+    "name": "door",
+    "points": 16,
+    "image": "overlay-tokens/doors/wood-door.png",
+    "xws": "wooddoor"
+  },
+  {
+    "name": "hot coals",
+    "points": 17,
+    "image": "overlay-tokens/hazardous-terrain/hot-coals.png",
+    "xws": "hotcoals"
+  },
+  {
+    "name": "thorns",
+    "points": 18,
+    "image": "overlay-tokens/hazardous-terrain/thorns.png",
+    "xws": "thorns"
+  },
+  {
+    "name": "altar",
+    "points": 19,
+    "image": "overlay-tokens/obstacles/altar.png",
+    "xws": "altar"
+  },
+  {
+    "name": "barrel",
+    "points": 20,
+    "image": "overlay-tokens/obstacles/barrel.png",
+    "xws": "barrel"
+  },
+  {
+    "name": "bookcase",
+    "points": 21,
+    "image": "overlay-tokens/obstacles/bookcase.png",
+    "xws": "bookcase"
+  },
+  {
+    "name": "boulder",
+    "points": 22,
+    "image": "overlay-tokens/obstacles/boulder-1.png",
+    "xws": "boulder"
+  },
+  {
+    "name": "boulder",
+    "points": 23,
+    "image": "overlay-tokens/obstacles/boulder-2.png",
+    "xws": "boulder"
+  },
+  {
+    "name": "boulder",
+    "points": 24,
+    "image": "overlay-tokens/obstacles/boulder-3.png",
+    "xws": "boulder"
+  },
+  {
+    "name": "bush",
+    "points": 25,
+    "image": "overlay-tokens/obstacles/bush.png",
+    "xws": "bush"
+  },
+  {
+    "name": "cabinet",
+    "points": 26,
+    "image": "overlay-tokens/obstacles/cabinet.png",
+    "xws": "cabinet"
+  },
+  {
+    "name": "crate",
+    "points": 27,
+    "image": "overlay-tokens/obstacles/crate.png",
+    "xws": "crate"
+  },
+  {
+    "name": "crystal",
+    "points": 28,
+    "image": "overlay-tokens/obstacles/crystal.png",
+    "xws": "crystal"
+  },
+  {
+    "name": "dark pit",
+    "points": 29,
+    "image": "overlay-tokens/obstacles/dark-pit.png",
+    "xws": "darkpit"
+  },
+  {
+    "name": "fountain",
+    "points": 30,
+    "image": "overlay-tokens/obstacles/fountain.png",
+    "xws": "fountain"
+  },
+  {
+    "name": "nest",
+    "points": 31,
+    "image": "overlay-tokens/obstacles/nest.png",
+    "xws": "nest"
+  },
+  {
+    "name": "rock column",
+    "points": 32,
+    "image": "overlay-tokens/obstacles/rock-column.png",
+    "xws": "rockcolumn"
+  },
+  {
+    "name": "sarcophagus",
+    "points": 33,
+    "image": "overlay-tokens/obstacles/sarcophagus.png",
+    "xws": "sarcophagus"
+  },
+  {
+    "name": "shelf",
+    "points": 34,
+    "image": "overlay-tokens/obstacles/shelf.png",
+    "xws": "shelf"
+  },
+  {
+    "name": "stalagmites",
+    "points": 35,
+    "image": "overlay-tokens/obstacles/stalagmites.png",
+    "xws": "stalagmites"
+  },
+  {
+    "name": "stone pillar",
+    "points": 36,
+    "image": "overlay-tokens/obstacles/stone-pillar.png",
+    "xws": "stonepillar"
+  },
+  {
+    "name": "stump",
+    "points": 37,
+    "image": "overlay-tokens/obstacles/stump.png",
+    "xws": "stump"
+  },
+  {
+    "name": "table",
+    "points": 38,
+    "image": "overlay-tokens/obstacles/table.png",
+    "xws": "table"
+  },
+  {
+    "name": "totem",
+    "points": 39,
+    "image": "overlay-tokens/obstacles/totem.png",
+    "xws": "totem"
+  },
+  {
+    "name": "tree",
+    "points": 40,
+    "image": "overlay-tokens/obstacles/tree.png",
+    "xws": "tree"
+  },
+  {
+    "name": "wall section",
+    "points": 41,
+    "image": "overlay-tokens/obstacles/wall-section.png",
+    "xws": "wallsection"
+  },
+  {
+    "name": "trap",
+    "points": 42,
+    "image": "overlay-tokens/traps/bear-trap.png",
+    "xws": "beartrap"
+  },
+  {
+    "name": "trap",
+    "points": 43,
+    "image": "overlay-tokens/traps/poison-gas-trap.png",
+    "xws": "poisongastrap"
+  },
+  {
+    "name": "trap",
+    "points": 44,
+    "image": "overlay-tokens/traps/spike-pit-trap.png",
+    "xws": "spikepittrap"
+  },
+  {
+    "name": "coin",
+    "points": 45,
+    "image": "overlay-tokens/treasures/coin-1.png",
+    "xws": "coin1"
+  },
+  {
+    "name": "coin",
+    "points": 46,
+    "image": "overlay-tokens/treasures/coin-5.png",
+    "xws": "coin5"
+  },
+  {
+    "name": "treasure",
+    "points": 47,
+    "image": "overlay-tokens/treasures/treasure.png",
+    "xws": "treasure"
+  }
+]
