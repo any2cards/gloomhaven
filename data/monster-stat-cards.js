c39defee762f66602bb3f798ee38e2d2f39d282c
any2cards <any2cards@yahoo.com>
2018-11-22 16:13:07 -0800

    Add Monster Stat Cards

---
+++ data/monster-stat-cards.js
@@ -0,0 +1,566 @@
+[
+  {
+    "name": "ancient artillary",
+    "points": 0,
+    "image": "monster-stat-cards/ancient-artillary-0.png",
+    "xws": "ancientartillary"
+  },
+  {
+    "name": "ancient artillary",
+    "points": 1,
+    "image": "monster-stat-cards/ancient-artillary-4.png",
+    "xws": "ancientartillary"
+  },
+  {
+    "name": "bandit archer",
+    "points": 2,
+    "image": "monster-stat-cards/bandit-archer-0.png",
+    "xws": "banditarcher"
+  },
+  {
+    "name": "bandit archer",
+    "points": 3,
+    "image": "monster-stat-cards/bandit-archer-4.png",
+    "xws": "banditarcher"
+  },
+  {
+    "name": "bandit commander",
+    "points": 4,
+    "image": "monster-stat-cards/bandit-commander-0.png",
+    "xws": "banditcommander"
+  },
+  {
+    "name": "bandit commander",
+    "points": 5,
+    "image": "monster-stat-cards/bandit-commander-4.png",
+    "xws": "banditcommander"
+  },
+  {
+    "name": "bandit guard",
+    "points": 6,
+    "image": "monster-stat-cards/bandit-guard-0.png",
+    "xws": "banditguard"
+  },
+  {
+    "name": "bandit guard",
+    "points": 7,
+    "image": "monster-stat-cards/bandit-guard-4.png",
+    "xws": "banditguard"
+  },
+  {
+    "name": "black imp",
+    "points": 8,
+    "image": "monster-stat-cards/black-imp-0.png",
+    "xws": "blackimp"
+  },
+  {
+    "name": "black imp",
+    "points": 9,
+    "image": "monster-stat-cards/black-imp-4.png",
+    "xws": "blackimp"
+  },
+  {
+    "name": "captain of the guard",
+    "points": 10,
+    "image": "monster-stat-cards/capatin-of-the-guard-0.png",
+    "xws": "captainoftheguard"
+  },
+  {
+    "name": "captain of the guard",
+    "points": 11,
+    "image": "monster-stat-cards/captain-of-the-guard-4.png",
+    "xws": "captainoftheguard"
+  },
+  {
+    "name": "cave bear",
+    "points": 12,
+    "image": "monster-stat-cards/cave-bear-0.png",
+    "xws": "cavebear"
+  },
+  {
+    "name": "cave bear",
+    "points": 13,
+    "image": "monster-stat-cards/cave-bear-4.png",
+    "xws": "cavebear"
+  },
+  {
+    "name": "city archer",
+    "points": 14,
+    "image": "monster-stat-cards/city-archer-0.png",
+    "xws": "cityarcher"
+  },
+  {
+    "name": "city archer",
+    "points": 15,
+    "image": "monster-stat-cards/city-archer-4.png",
+    "xws": "cityarcher"
+  },
+  {
+    "name": "city guard",
+    "points": 16,
+    "image": "monster-stat-cards/city-guard-0.png",
+    "xws": "cityguard"
+  },
+  {
+    "name": "city guard",
+    "points": 17,
+    "image": "monster-stat-cards/city-guard-4.png",
+    "xws": "cityguard"
+  },
+  {
+    "name": "cultist",
+    "points": 18,
+    "image": "monster-stat-cards/cultist-0.png",
+    "xws": "cultist"
+  },
+  {
+    "name": "cultist",
+    "points": 19,
+    "image": "monster-stat-cards/cultist-4.png",
+    "xws": "cultist"
+  },
+  {
+    "name": "dark rider",
+    "points": 20,
+    "image": "monster-stat-cards/dark-rider-0.png",
+    "xws": "darkrider"
+  },
+  {
+    "name": "dark rider",
+    "points": 21,
+    "image": "monster-stat-cards/dark-rider-4.png",
+    "xws": "darkrider"
+  },
+  {
+    "name": "deep terror",
+    "points": 22,
+    "image": "monster-stat-cards/deep-terror-0.png",
+    "xws": "deepterror"
+  },
+  {
+    "name": "deep terror",
+    "points": 23,
+    "image": "monster-stat-cards/deep-terror-4.png",
+    "xws": "deepterror"
+  },
+  {
+    "name": "earth demon",
+    "points": 24,
+    "image": "monster-stat-cards/earth-demon-0.png",
+    "xws": "earthdemon"
+  },
+  {
+    "name": "earth demon",
+    "points": 25,
+    "image": "monster-stat-cards/earth-demon-4.png",
+    "xws": "earthdemon"
+  },
+  {
+    "name": "elder drake",
+    "points": 26,
+    "image": "monster-stat-cards/elder-drake-0.png",
+    "xws": "elderdrake"
+  },
+  {
+    "name": "elder drake",
+    "points": 27,
+    "image": "monster-stat-cards/elder-drake-4.png",
+    "xws": "elderdrake"
+  },
+  {
+    "name": "flame demon",
+    "points": 28,
+    "image": "monster-stat-cards/flame-demon-0.png",
+    "xws": "flamedemon"
+  },
+  {
+    "name": "flame demon",
+    "points": 29,
+    "image": "monster-stat-cards/flame-demon-4.png",
+    "xws": "flamedemon"
+  },
+  {
+    "name": "forest imp",
+    "points": 30,
+    "image": "monster-stat-cards/forest-imp-0.png",
+    "xws": "forestimp"
+  },
+  {
+    "name": "forest imp",
+    "points": 31,
+    "image": "monster-stat-cards/forest-imp-4.png",
+    "xws": "forestimp"
+  },
+  {
+    "name": "frost demon",
+    "points": 32,
+    "image": "monster-stat-cards/frost-demon-0.png",
+    "xws": "frostdemon"
+  },
+  {
+    "name": "frost demon",
+    "points": 33,
+    "image": "monster-stat-cards/frost-demon-4.png",
+    "xws": "frostdemon"
+  },
+  {
+    "name": "giant viper",
+    "points": 34,
+    "image": "monster-stat-cards/giant-viper-0.png",
+    "xws": "giantviper"
+  },
+  {
+    "name": "giant viper",
+    "points": 35,
+    "image": "monster-stat-cards/giant-viper-4.png",
+    "xws": "giantviper"
+  },
+  {
+    "name": "harrower infester",
+    "points": 36,
+    "image": "monster-stat-cards/harrower-infester-0.png",
+    "xws": "harrowerinfester"
+  },
+  {
+    "name": "harrower infester",
+    "points": 37,
+    "image": "monster-stat-cards/harrower-infester-4.png",
+    "xws": "harrowerinfester"
+  },
+  {
+    "name": "hound",
+    "points": 38,
+    "image": "monster-stat-cards/hound-0.png",
+    "xws": "hound"
+  },
+  {
+    "name": "hound",
+    "points": 39,
+    "image": "monster-stat-cards/hound-4.png",
+    "xws": "hound"
+  },
+  {
+    "name": "inox archer",
+    "points": 40,
+    "image": "monster-stat-cards/inox-archer-0.png",
+    "xws": "inoxarcher"
+  },
+  {
+    "name": "inox archer",
+    "points": 41,
+    "image": "monster-stat-cards/inox-archer-4.png",
+    "xws": "inoxarcher"
+  },
+  {
+    "name": "inox bodyguard",
+    "points": 42,
+    "image": "monster-stat-cards/inox-bodyguard-0.png",
+    "xws": "inoxbodyguard"
+  },
+  {
+    "name": "inox bodyguard",
+    "points": 43,
+    "image": "monster-stat-cards/inox-bodyguard-4.png",
+    "xws": "inoxbodyguard"
+  },
+  {
+    "name": "inox guard",
+    "points": 44,
+    "image": "monster-stat-cards/inox-guard-0.png",
+    "xws": "inoxguard"
+  },
+  {
+    "name": "inox guard",
+    "points": 45,
+    "image": "monster-stat-cards/inox-guard-4.png",
+    "xws": "inoxguard"
+  },
+  {
+    "name": "inox shaman",
+    "points": 46,
+    "image": "monster-stat-cards/inox-shaman-0.png",
+    "xws": "inoxshaman"
+  },
+  {
+    "name": "inox shaman",
+    "points": 47,
+    "image": "monster-stat-cards/inox-shaman-4.png",
+    "xws": "inoxshaman"
+  },
+  {
+    "name": "jekserah",
+    "points": 48,
+    "image": "monster-stat-cards/jekserah-0.png",
+    "xws": "jekserah"
+  },
+  {
+    "name": "jekserah",
+    "points": 49,
+    "image": "monster-stat-cards/jekserah-4.png",
+    "xws": "jekserah"
+  },
+  {
+    "name": "living bones",
+    "points": 50,
+    "image": "monster-stat-cards/living-bones-0.png",
+    "xws": "livingbones"
+  },
+  {
+    "name": "living bones",
+    "points": 51,
+    "image": "monster-stat-cards/living-bones-4.png",
+    "xws": "livingbones"
+  },
+  {
+    "name": "living corpse",
+    "points": 52,
+    "image": "monster-stat-cards/living-corpse-0.png",
+    "xws": "livingcorpse"
+  },
+  {
+    "name": "living corpse",
+    "points": 53,
+    "image": "monster-stat-cards/living-corpse-4.png",
+    "xws": "livingcorpse"
+  },
+  {
+    "name": "living spirit",
+    "points": 54,
+    "image": "monster-stat-cards/living-spirit-0.png",
+    "xws": "livingspirit"
+  },
+  {
+    "name": "living spirit",
+    "points": 55,
+    "image": "monster-stat-cards/living-spirit-4.png",
+    "xws": "livingspirit"
+  },
+  {
+    "name": "lurker",
+    "points": 56,
+    "image": "monster-stat-cards/lurker-0.png",
+    "xws": "lurker"
+  },
+  {
+    "name": "lurker",
+    "points": 57,
+    "image": "monster-stat-cards/lurker-4.png",
+    "xws": "lurker"
+  },
+  {
+    "name": "merciless overseer",
+    "points": 58,
+    "image": "monster-stat-cards/merciless-overseer-0.png",
+    "xws": "mercilessoverseer"
+  },
+  {
+    "name": "merciless overseer",
+    "points": 59,
+    "image": "monster-stat-cards/merciless-overseer-4.png",
+    "xws": "mercilessoverseer"
+  },
+  {
+    "name": "night demon",
+    "points": 60,
+    "image": "monster-stat-cards/night-demon-0.png",
+    "xws": "nightdemon"
+  },
+  {
+    "name": "night demon",
+    "points": 61,
+    "image": "monster-stat-cards/night-demon-4.png",
+    "xws": "nightdemon"
+  },
+  {
+    "name": "ooze",
+    "points": 62,
+    "image": "monster-stat-cards/ooze-0.png",
+    "xws": "ooze"
+  },
+  {
+    "name": "ooze",
+    "points": 63,
+    "image": "monster-stat-cards/ooze-4.png",
+    "xws": "ooze"
+  },
+  {
+    "name": "prime demon",
+    "points": 64,
+    "image": "monster-stat-cards/prime-demon-0.png",
+    "xws": "primedemon"
+  },
+  {
+    "name": "prime demon",
+    "points": 65,
+    "image": "monster-stat-cards/prime-demon-4.png",
+    "xws": "primedemon"
+  },
+  {
+    "name": "rending drake",
+    "points": 66,
+    "image": "monster-stat-cards/rending-drake-0.png",
+    "xws": "rendingdrake"
+  },
+  {
+    "name": "rending drake",
+    "points": 67,
+    "image": "monster-stat-cards/rending-drake-4.png",
+    "xws": "rendingdrake"
+  },
+  {
+    "name": "savvas icestorm",
+    "points": 68,
+    "image": "monster-stat-cards/savvas-icestorm-0.png",
+    "xws": "savvasicestorm"
+  },
+  {
+    "name": "savvas icestorm",
+    "points": 69,
+    "image": "monster-stat-cards/savvas-icestorm-4.png",
+    "xws": "savvasicestorm"
+  },
+  {
+    "name": "savvas lavaflow",
+    "points": 70,
+    "image": "monster-stat-cards/savvas-lavaflow-0.png",
+    "xws": "savvaslavaflow"
+  },
+  {
+    "name": "savvas lavaflow",
+    "points": 71,
+    "image": "monster-stat-cards/savvas-lavaflow-4.png",
+    "xws": "savvaslavaflow"
+  },
+  {
+    "name": "spitting drake",
+    "points": 72,
+    "image": "monster-stat-cards/spitting-drake-0.png",
+    "xws": "spittingdrake"
+  },
+  {
+    "name": "spitting drake",
+    "points": 73,
+    "image": "monster-stat-cards/spitting-drake-4.png",
+    "xws": "spittingdrake"
+  },
+  {
+    "name": "stone elemental",
+    "points": 74,
+    "image": "monster-stat-cards/stone-elemental-0.png",
+    "xws": "stoneelemental"
+  },
+  {
+    "name": "stone elemental",
+    "points": 75,
+    "image": "monster-stat-cards/stone-elemental-4.png",
+    "xws": "stoneelemental"
+  },
+  {
+    "name": "sum demon",
+    "points": 76,
+    "image": "monster-stat-cards/sun-demon-0.png",
+    "xws": "sundemon"
+  },
+  {
+    "name": "sun demon",
+    "points": 77,
+    "image": "monster-stat-cards/sun-demon-4.png",
+    "xws": "sundemon"
+  },
+  {
+    "name": "the betrayer",
+    "points": 78,
+    "image": "monster-stat-cards/the-betrayer-0.png",
+    "xws": "thebetrayer"
+  },
+  {
+    "name": "the betrayer",
+    "points": 79,
+    "image": "monster-stat-cards/the-betrayer-4.png",
+    "xws": "thebetrayer"
+  },
+  {
+    "name": "the colorless",
+    "points": 80,
+    "image": "monster-stat-cards/the-colorless-0.png",
+    "xws": "thecolorless"
+  },
+  {
+    "name": "the colorless",
+    "points": 81,
+    "image": "monster-stat-cards/the-colorless-4.png",
+    "xws": "thecolorless"
+  },
+  {
+    "name": "the gloom",
+    "points": 82,
+    "image": "monster-stat-cards/the-gloom-0.png",
+    "xws": "thegloom"
+  },
+  {
+    "name": "the gloom",
+    "points": 83,
+    "image": "monster-stat-cards/the-gloom-4.png",
+    "xws": "thegloom"
+  },
+  {
+    "name": "the sightless eye",
+    "points": 84,
+    "image": "monster-stat-cards/the-sightless-eye-0.png",
+    "xws": "thesightlesseye"
+  },
+  {
+    "name": "the sightless eye",
+    "points": 85,
+    "image": "monster-stat-cards/the-sightless-eye-4.png",
+    "xws": "thesightlesseye"
+  },
+  {
+    "name": "vermling scout",
+    "points": 86,
+    "image": "monster-stat-cards/vermling-scout-0.png",
+    "xws": "vermlingscout"
+  },
+  {
+    "name": "vermling scout",
+    "points": 87,
+    "image": "monster-stat-cards/vermling-scout-4.png",
+    "xws": "vermlingscout"
+  },
+  {
+    "name": "vermling shaman",
+    "points": 88,
+    "image": "monster-stat-cards/vermling-shaman-0.png",
+    "xws": "vermlingshaman"
+  },
+  {
+    "name": "vermling shaman",
+    "points": 89,
+    "image": "monster-stat-cards/vermling-shaman-4.png",
+    "xws": "vermlingshaman"
+  },
+  {
+    "name": "wind demon",
+    "points": 90,
+    "image": "monster-stat-cards/wind-demon-0.png",
+    "xws": "winddemon"
+  },
+  {
+    "name": "wind demon",
+    "points": 91,
+    "image": "monster-stat-cards/wind-demon-4.png",
+    "xws": "winddemon"
+  },
+  {
+    "name": "winged horror",
+    "points": 92,
+    "image": "monster-stat-cards/winged-horror-0.png",
+    "xws": "wingedhorror"
+  },
+  {
+    "name": "winged horror",
+    "points": 93,
+    "image": "monster-stat-cards/winged-horror-4.png",
+    "xws": "wingedhorror"
+  }
+]
