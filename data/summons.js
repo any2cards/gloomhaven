ed675655d563575b3968dd1dc3f36d8111372697
any2cards <any2cards@yahoo.com>
2018-11-23 22:23:03 -0800

    Fix Summons

--- data/summons.js
+++ data/summons.js
@@ -2,49 +2,7 @@
   {
     "name": "summons",
     "points": 0,
-    "image": "summons/blue1.png",
-    "xws": "summons"
-  },
-  {
-    "name": "summons",
-    "points": 1,
-    "image": "summons/green1.png",
-    "xws": "summons"
-  },
-  {
-    "name": "summons",
-    "points": 2,
-    "image": "summons/grey1.png",
-    "xws": "summons"
-  },
-  {
-    "name": "summons",
-    "points": 3,
-    "image": "summons/magenta1.png",
-    "xws": "summons"
-  },
-  {
-    "name": "summons",
-    "points": 4,
-    "image": "summons/orange1.png",
-    "xws": "summons"
-  },
-  {
-    "name": "summons",
-    "points": 5,
-    "image": "summons/purple1.png",
-    "xws": "summons"
-  },
-  {
-    "name": "summons",
-    "points": 6,
-    "image": "summons/red1.png",
-    "xws": "summons"
-  },
-  {
-    "name": "summons",
-    "points": 7,
-    "image": "summons/yellow1.png",
+    "image": "summons/summons.png",
     "xws": "summons"
   }
 ]
