a521eded359b76d092cb50f01a88958bccf11f24
any2cards <any2cards@yahoo.com>
2018-11-23 21:46:22 -0800

    Fix Map Tiles

--- data/map-tiles.js
+++ data/map-tiles.js
@@ -2,361 +2,361 @@
   {
     "name": "a1a",
     "points": 0,
-    "image": "world-map/a1a.png",
+    "image": "map-tiles/a1a.png",
     "xws": "a1a"
   },
   {
     "name": "a1b",
     "points": 1,
-    "image": "world-map/a1b.png",
+    "image": "map-tiles/a1b.png",
     "xws": "a1b"
   },
   {
     "name": "a2a",
     "points": 2,
-    "image": "world-map/a2a.png",
+    "image": "map-tiles/a2a.png",
     "xws": "a2a"
   },
   {
     "name": "a2b",
     "points": 3,
-    "image": "world-map/a2b.png",
+    "image": "map-tiles/a2b.png",
     "xws": "a2b"
   },
   {
     "name": "a3a",
     "points": 4,
-    "image": "world-map/a3a.png",
+    "image": "map-tiles/a3a.png",
     "xws": "a3a"
   },
   {
     "name": "a3b",
     "points": 5,
-    "image": "world-map/a3b.png",
+    "image": "map-tiles/a3b.png",
     "xws": "a3b"
   },
   {
     "name": "a4a",
     "points": 6,
-    "image": "world-map/a4a.png",
+    "image": "map-tiles/a4a.png",
     "xws": "a4a"
   },
   {
     "name": "a4b",
     "points": 7,
-    "image": "world-map/a4b.png",
+    "image": "map-tiles/a4b.png",
     "xws": "a4b"
   },
   {
     "name": "b1a",
     "points": 8,
-    "image": "world-map/b1a.png",
+    "image": "map-tiles/b1a.png",
     "xws": "b1a"
   },
   {
     "name": "b1b",
     "points": 9,
-    "image": "world-map/b1b.png",
+    "image": "map-tiles/b1b.png",
     "xws": "b1b"
   },
   {
     "name": "b2a",
     "points": 10,
-    "image": "world-map/b2a.png",
+    "image": "map-tiles/b2a.png",
     "xws": "b2a"
   },
   {
     "name": "b2b",
     "points": 11,
-    "image": "world-map/b2b.png",
+    "image": "map-tiles/b2b.png",
     "xws": "b2b"
   },
   {
     "name": "b3a",
     "points": 12,
-    "image": "world-map/b3a.png",
+    "image": "map-tiles/b3a.png",
     "xws": "b3a"
   },
   {
     "name": "b3b",
     "points": 13,
-    "image": "world-map/b3b.png",
+    "image": "map-tiles/b3b.png",
     "xws": "b3b"
   },
   {
     "name": "b4a",
     "points": 14,
-    "image": "world-map/b4a.png",
+    "image": "map-tiles/b4a.png",
     "xws": "b4a"
   },
   {
     "name": "b4b",
     "points": 15,
-    "image": "world-map/b4b.png",
+    "image": "map-tiles/b4b.png",
     "xws": "b4b"
   },
   {
     "name": "c1a",
     "points": 16,
-    "image": "world-map/c1a.png",
+    "image": "map-tiles/c1a.png",
     "xws": "c1a"
   },
   {
     "name": "c1b",
     "points": 17,
-    "image": "world-map/c1b.png",
+    "image": "map-tiles/c1b.png",
     "xws": "c1b"
   },
   {
     "name": "c2a",
     "points": 18,
-    "image": "world-map/c2a.png",
+    "image": "map-tiles/c2a.png",
     "xws": "c2a"
   },
   {
     "name": "c2b",
     "points": 19,
-    "image": "world-map/c2b.png",
+    "image": "map-tiles/c2b.png",
     "xws": "c2b"
   },
   {
     "name": "d1a",
     "points": 20,
-    "image": "world-map/d1a.png",
+    "image": "map-tiles/d1a.png",
     "xws": "d1a"
   },
   {
     "name": "d1b",
     "points": 21,
-    "image": "world-map/d1b.png",
+    "image": "map-tiles/d1b.png",
     "xws": "d1b"
   },
   {
     "name": "d2a",
     "points": 22,
-    "image": "world-map/d2a.png",
+    "image": "map-tiles/d2a.png",
     "xws": "d2a"
   },
   {
     "name": "d2b",
     "points": 23,
-    "image": "world-map/d2b.png",
+    "image": "map-tiles/d2b.png",
     "xws": "d2b"
   },
   {
     "name": "e1a",
     "points": 24,
-    "image": "world-map/e1a.png",
+    "image": "map-tiles/e1a.png",
     "xws": "e1a"
   },
   {
     "name": "e1b",
     "points": 25,
-    "image": "world-map/e1b.png",
+    "image": "map-tiles/e1b.png",
     "xws": "e1b"
   },
   {
     "name": "f1a",
     "points": 26,
-    "image": "world-map/f1a.png",
+    "image": "map-tiles/f1a.png",
     "xws": "f1a"
   },
   {
     "name": "f1b",
     "points": 27,
-    "image": "world-map/f1b.png",
+    "image": "map-tiles/f1b.png",
     "xws": "f1b"
   },
   {
     "name": "g1a",
     "points": 28,
-    "image": "world-map/g1a.png",
+    "image": "map-tiles/g1a.png",
     "xws": "g1a"
   },
   {
     "name": "g1b",
     "points": 29,
-    "image": "world-map/g1b.png",
+    "image": "map-tiles/g1b.png",
     "xws": "g1b"
   },
   {
     "name": "g2a",
     "points": 30,
-    "image": "world-map/g2a.png",
+    "image": "map-tiles/g2a.png",
     "xws": "g2a"
   },
   {
     "name": "g2b",
     "points": 31,
-    "image": "world-map/g2b.png",
+    "image": "map-tiles/g2b.png",
     "xws": "g2b"
   },
   {
     "name": "h1a",
     "points": 32,
-    "image": "world-map/h1a.png",
+    "image": "map-tiles/h1a.png",
     "xws": "h1a"
   },
   {
     "name": "h1b",
     "points": 33,
-    "image": "world-map/h1b.png",
+    "image": "map-tiles/h1b.png",
     "xws": "h1b"
   },
   {
     "name": "h2a",
     "points": 34,
-    "image": "world-map/h2a.png",
+    "image": "map-tiles/h2a.png",
     "xws": "h2a"
   },
   {
     "name": "h2b",
     "points": 35,
-    "image": "world-map/h2b.png",
+    "image": "map-tiles/h2b.png",
     "xws": "h2b"
   },
   {
     "name": "h3a",
     "points": 36,
-    "image": "world-map/h3a.png",
+    "image": "map-tiles/h3a.png",
     "xws": "h3a"
   },
   {
     "name": "h3b",
     "points": 37,
-    "image": "world-map/h3b.png",
+    "image": "map-tiles/h3b.png",
     "xws": "h3b"
   },
   {
     "name": "i1a",
     "points": 38,
-    "image": "world-map/i1a.png",
+    "image": "map-tiles/i1a.png",
     "xws": "i1a"
   },
   {
     "name": "i1b",
     "points": 39,
-    "image": "world-map/i1b.png",
+    "image": "map-tiles/i1b.png",
     "xws": "i1b"
   },
   {
     "name": "i2a",
     "points": 40,
-    "image": "world-map/i2a.png",
+    "image": "map-tiles/i2a.png",
     "xws": "i2a"
   },
   {
     "name": "i2b",
     "points": 41,
-    "image": "world-map/i2b.png",
+    "image": "map-tiles/i2b.png",
     "xws": "i2b"
   },
   {
     "name": "j1a",
     "points": 42,
-    "image": "world-map/j1a.png",
+    "image": "map-tiles/j1a.png",
     "xws": "j1a"
   },
   {
     "name": "j1b",
     "points": 43,
-    "image": "world-map/j1b.png",
+    "image": "map-tiles/j1b.png",
     "xws": "j1b"
   },
   {
     "name": "j2a",
     "points": 44,
-    "image": "world-map/j2a.png",
+    "image": "map-tiles/j2a.png",
     "xws": "j2a"
   },
   {
     "name": "j2b",
     "points": 45,
-    "image": "world-map/j2b.png",
+    "image": "map-tiles/j2b.png",
     "xws": "j2b"
   },
   {
     "name": "k1a",
     "points": 46,
-    "image": "world-map/k1a.png",
+    "image": "map-tiles/k1a.png",
     "xws": "k1a"
   },
   {
     "name": "k1b",
     "points": 47,
-    "image": "world-map/k1b.png",
+    "image": "map-tiles/k1b.png",
     "xws": "k1b"
   },
   {
     "name": "k2a",
     "points": 48,
-    "image": "world-map/k2a.png",
+    "image": "map-tiles/k2a.png",
     "xws": "k2a"
   },
   {
     "name": "k2b",
     "points": 49,
-    "image": "world-map/k2b.png",
+    "image": "map-tiles/k2b.png",
     "xws": "k2b"
   },
   {
     "name": "l1a",
     "points": 50,
-    "image": "world-map/l1a.png",
+    "image": "map-tiles/l1a.png",
     "xws": "l1a"
   },
   {
     "name": "l1b",
     "points": 51,
-    "image": "world-map/l1b.png",
+    "image": "map-tiles/l1b.png",
     "xws": "l1b"
   },
   {
     "name": "l2a",
     "points": 52,
-    "image": "world-map/l2a.png",
+    "image": "map-tiles/l2a.png",
     "xws": "l2a"
   },
   {
     "name": "l2b",
     "points": 53,
-    "image": "world-map/l2b.png",
+    "image": "map-tiles/l2b.png",
     "xws": "l2b"
   },
   {
     "name": "l3a",
     "points": 54,
-    "image": "world-map/l3a.png",
+    "image": "map-tiles/l3a.png",
     "xws": "l3a"
   },
   {
     "name": "l3b",
     "points": 55,
-    "image": "world-map/l3b.png",
+    "image": "map-tiles/l3b.png",
     "xws": "l3b"
   },
   {
     "name": "m1a",
     "points": 56,
-    "image": "world-map/m1a.png",
+    "image": "map-tiles/m1a.png",
     "xws": "m1a"
   },
   {
     "name": "m1b",
     "points": 57,
-    "image": "world-map/m1b.png",
+    "image": "map-tiles/m1b.png",
     "xws": "m1b"
   },
   {
     "name": "n1a",
     "points": 58,
-    "image": "world-map/n1a.png",
+    "image": "map-tiles/n1a.png",
     "xws": "n1a"
   },
   {
     "name": "n1b",
     "points": 59,
-    "image": "world-map/n1b.png",
+    "image": "map-tiles/n1b.png",
     "xws": "n1b"
   }
 ]
