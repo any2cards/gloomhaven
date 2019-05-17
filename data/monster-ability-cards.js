edf212b4c9d3fca6e91c9d4337614fc96d57c64e
any2cards <any2cards@yahoo.com>
2019-05-17 14:39:05 -0700

    Implemeny Diviner Assets - Final Version

--- data/monster-ability-cards.js
+++ data/monster-ability-cards.js
@@ -1842,7 +1842,7 @@
     "xws": "unfathomablefortitude"
   },
   {
-    "name": "whats yours is mine",
+    "name": "what's yours is mine",
     "points": 307,
     "image": "monster-ability-cards/manifestation-of-corruption/whats-yours-is-mine.png",
     "xws": "whatsyoursismine"
