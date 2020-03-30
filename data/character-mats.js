ceff71c3988e6684f9642b78bc473fa84e2754ef
any2cards <any2cards@yahoo.com>
2020-03-30 11:38:10 -0700

    Implement Character Mat Backs

--- data/character-mats.js
+++ data/character-mats.js
@@ -6,111 +6,225 @@
     "xws": "beasttyrant"
   },
   {
-    "name": "berserker",
+    "name": "beast tyrant",
     "points": 1,
+    "image": "character-mats/beast-tyrant-back.png",
+    "xws": "beasttyrant"
+  },
+  {
+    "name": "berserker",
+    "points": 2,
     "image": "character-mats/berserker.png",
     "xws": "berserker"
   },
   {
+    "name": "berserker",
+    "points": 3,
+    "image": "character-mats/berserker-back.png",
+    "xws": "berserker"
+  },
+  {
     "name": "bladeswarm",
-    "points": 2,
+    "points": 4,
     "image": "character-mats/bladeswarm.png",
     "xws": "bladeswarm"
   },
   {
+    "name": "bladeswarm",
+    "points": 5,
+    "image": "character-mats/bladeswarm-back.png",
+    "xws": "bladeswarm"
+  },
+  {
     "name": "brute",
-    "points": 3,
+    "points": 6,
     "image": "character-mats/brute.png",
     "xws": "brute"
   },
   {
+    "name": "brute",
+    "points": 7,
+    "image": "character-mats/brute-back.png",
+    "xws": "brute"
+  },
+  {
     "name": "cragheart",
-    "points": 4,
+    "points": 8,
     "image": "character-mats/cragheart.png",
     "xws": "cragheart"
   },
   {
+    "name": "cragheart",
+    "points": 9,
+    "image": "character-mats/cragheart-back.png",
+    "xws": "cragheart"
+  },
+  {
     "name": "doomstalker",
-    "points": 5,
+    "points": 10,
     "image": "character-mats/doomstalker.png",
     "xws": "doomstalker"
   },
   {
+    "name": "doomstalker",
+    "points": 11,
+    "image": "character-mats/doomstalker-back.png",
+    "xws": "doomstalker"
+  },
+  {
     "name": "elementalist",
-    "points": 6,
+    "points": 12,
     "image": "character-mats/elementalist.png",
     "xws": "elementalist"
   },
   {
+    "name": "elementalist",
+    "points": 13,
+    "image": "character-mats/elementalist-back.png",
+    "xws": "elementalist"
+  },
+  {
     "name": "mindthief",
-    "points": 7,
+    "points": 14,
     "image": "character-mats/mindthief.png",
     "xws": "mindthief"
   },
   {
+    "name": "mindthief",
+    "points": 15,
+    "image": "character-mats/mindthief-back.png",
+    "xws": "mindthief"
+  },
+  {
     "name": "nightshroud",
-    "points": 8,
+    "points": 16,
     "image": "character-mats/nightshroud.png",
     "xws": "nightshroud"
   },
   {
+    "name": "nightshroud",
+    "points": 17,
+    "image": "character-mats/nightshroud-back.png",
+    "xws": "nightshroud"
+  },
+  {
     "name": "plagueherald",
-    "points": 9,
+    "points": 18,
     "image": "character-mats/plagueherald.png",
     "xws": "plagueherald"
   },
   {
+    "name": "plagueherald",
+    "points": 19,
+    "image": "character-mats/plagueherald-back.png",
+    "xws": "plagueherald"
+  },
+  {
     "name": "quartermaster",
-    "points": 10,
+    "points": 20,
     "image": "character-mats/quartermaster.png",
     "xws": "quartermaster"
   },
   {
+    "name": "quartermaster",
+    "points": 21,
+    "image": "character-mats/quartermaster-back.png",
+    "xws": "quartermaster"
+  },
+  {
     "name": "sawbones",
-    "points": 11,
+    "points": 22,
     "image": "character-mats/sawbones.png",
     "xws": "sawbones"
   },
   {
+    "name": "sawbones",
+    "points": 23,
+    "image": "character-mats/sawbones-back.png",
+    "xws": "sawbones"
+  },
+  {
     "name": "scoundrel",
-    "points": 12,
+    "points": 24,
     "image": "character-mats/scoundrel.png",
     "xws": "scoundrel"
   },
   {
+    "name": "scoundrel",
+    "points": 25,
+    "image": "character-mats/scoundrel-back.png",
+    "xws": "scoundrel"
+  },
+  {
     "name": "soothsinger",
-    "points": 13,
+    "points": 26,
     "image": "character-mats/soothsinger.png",
     "xws": "soothsinger"
   },
   {
+    "name": "soothsinger",
+    "points": 27,
+    "image": "character-mats/soothsinger-back.png",
+    "xws": "soothsinger"
+  },
+  {
     "name": "spellweaver",
-    "points": 14,
+    "points": 28,
     "image": "character-mats/spellweaver.png",
     "xws": "spellweaver"
   },
   {
+    "name": "spellweaver",
+    "points": 29,
+    "image": "character-mats/spellweaver-back.png",
+    "xws": "spellweaver"
+  },
+  {
     "name": "summoner",
-    "points": 15,
+    "points": 30,
     "image": "character-mats/summoner.png",
     "xws": "summoner"
   },
   {
+    "name": "summoner",
+    "points": 31,
+    "image": "character-mats/summoner-back.png",
+    "xws": "summoner"
+  },
+  {
     "name": "sunkeeper",
-    "points": 16,
+    "points": 32,
     "image": "character-mats/sunkeeper.png",
     "xws": "sunkeeper"
   },
   {
+    "name": "sunkeeper",
+    "points": 33,
+    "image": "character-mats/sunkeeper-back.png",
+    "xws": "sunkeeper"
+  },
+  {
     "name": "tinkerer",
-    "points": 17,
+    "points": 34,
     "image": "character-mats/tinkerer.png",
     "xws": "tinkerer"
   },
   {
+    "name": "tinkerer",
+    "points": 35,
+    "image": "character-mats/tinkerer-back.png",
+    "xws": "tinkerer"
+  },
+  {
     "name": "diviner",
-    "points": 18,
+    "points": 36,
     "image": "character-mats/diviner.png",
+    "xws": "diviner"
+  },
+  {
+    "name": "diviner",
+    "points": 37,
+    "image": "character-mats/diviner-back.png",
     "xws": "diviner"
   }
 ]
