03d2dbe353b83a30557886d1b4db4e010bc416e9
any2cards <any2cards@yahoo.com>
2021-03-11 12:21:54 -0800

    Implement 3P FC Player Aid Cards

--- data/player-aid-cards.js
+++ data/player-aid-cards.js
@@ -7,32 +7,44 @@
   },
   {
     "name": "halt bottom back",
-    "points": 0,
+    "points": 1,
     "image": "character-ability-cards-revised/ZZ/halt-bottom-back.png",
     "xws": "haltbottomback"
   },
   {
     "name": "halt top front",
-    "points": 0,
+    "points": 2,
     "image": "character-ability-cards-revised/ZZ/halt-top-front.png",
     "xws": "halttopfront"
   },
   {
     "name": "halt top back",
-    "points": 0,
+    "points": 3,
     "image": "character-ability-cards-revised/ZZ/halt-top-back.png",
     "xws": "halttopback"
   },
   {
     "name": "player reference back",
-    "points": 0,
-    "image": "player-aid-cards/player-reference-back.png",
+    "points": 4,
+    "image": "player-aid-cards/gh-player-reference-back.png",
     "xws": "playerreferenceback"
   },
   {
     "name": "player reference front",
-    "points": 1,
-    "image": "player-aid-cards/player-reference-front.png",
+    "points": 5,
+    "image": "player-aid-cards/gh-player-reference-front.png",
+    "xws": "playerreferencefront"
+  },
+  {
+    "name": "player reference back",
+    "points": 6,
+    "image": "player-aid-cards/fc-player-reference-back.png",
+    "xws": "playerreferenceback"
+  },
+  {
+    "name": "player reference front",
+    "points": 7,
+    "image": "player-aid-cards/fc-player-reference-front.png",
     "xws": "playerreferencefront"
   }
 ]
