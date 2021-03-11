32e705709f8938bdd4de9e4a710bc426b010059e
any2cards <any2cards@yahoo.com>
2021-03-11 07:14:56 -0800

    Implement 3P FC Player Aid Cards

--- data/player-aid-cards.js
+++ data/player-aid-cards.js
@@ -1,5 +1,29 @@
 [
   {
+    "name": "halt bottom front",
+    "points": 0,
+    "image": "character-ability-cards-revised/ZZ/halt-bottom-front.png",
+    "xws": "haltbottomfront"
+  },
+  {
+    "name": "halt bottom back",
+    "points": 0,
+    "image": "character-ability-cards-revised/ZZ/halt-bottom-back.png",
+    "xws": "haltbottomback"
+  },
+  {
+    "name": "halt top front",
+    "points": 0,
+    "image": "character-ability-cards-revised/ZZ/halt-top-front.png",
+    "xws": "halttopfront"
+  },
+  {
+    "name": "halt top back",
+    "points": 0,
+    "image": "character-ability-cards-revised/ZZ/halt-top-back.png",
+    "xws": "halttopback"
+  },
+  {
     "name": "player reference back",
     "points": 0,
     "image": "player-aid-cards/player-reference-back.png",
