a51014c13389e95bc18db553845dcfd0ed0d96d8
any2cards <any2cards@yahoo.com>
2021-11-04 14:30:07 -0700

    Implement Expansions Value

--- data/overlay-tokens.js
+++ data/overlay-tokens.js
@@ -1,289 +1,344 @@
 [
   {
-    "name": "earth",
+    "name": "rift",
     "points": 0,
+    "expansion": "Forgotten Circles",
+    "image": "overlay-tokens/character-toens/diviner-rift.png",
+    "xws": "rift"
+  },
+  {
+    "name": "earth",
+    "points": 1,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/earth-1.png",
     "xws": "earth"
   },
   {
     "name": "earth",
-    "points": 1,
+    "points": 2,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/earth-2.png",
     "xws": "earth"
   },
   {
     "name": "stone",
-    "points": 2,
+    "points": 3,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/man-made-stone-1.png",
     "xws": "manmadestone"
   },
   {
     "name": "stone",
-    "points": 3,
+    "points": 4,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/man-made-stone-2.png",
     "xws": "manmadestone"
   },
   {
     "name": "stone",
-    "points": 4,
+    "points": 5,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/natural-stone-1.png",
     "xws": "naturalstone"
   },
   {
     "name": "stone",
-    "points": 5,
+    "points": 6,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/natural-stone-2.png",
     "xws": "naturalstone"
   },
   {
     "name": "pressure plate",
-    "points": 6,
+    "points": 7,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/pressure-plate.png",
     "xws": "pressureplate"
   },
   {
     "name": "wood",
-    "points": 7,
+    "points": 8,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/wood-1.png",
     "xws": "wood"
   },
   {
     "name": "wood",
-    "points": 8,
+    "points": 9,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/corridors/wood-2.png",
     "xws": "wood"
   },
   {
     "name": "log",
-    "points": 9,
+    "points": 10,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/difficult-terrain/log.png",
     "xws": "log"
   },
   {
     "name": "rubble",
-    "points": 10,
+    "points": 11,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/difficult-terrain/rubble.png",
     "xws": "rubble"
   },
   {
     "name": "stairs",
-    "points": 11,
+    "points": 12,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/difficult-terrain/stairs.png",
     "xws": "stairs"
   },
   {
     "name": "water",
-    "points": 12,
+    "points": 13,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/difficult-terrain/water.png",
     "xws": "water"
   },
   {
     "name": "door",
-    "points": 13,
+    "points": 14,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/doors/dark-fog.png",
     "xws": "darkfog"
   },
   {
     "name": "door",
-    "points": 14,
+    "points": 15,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/doors/light-fog.png",
     "xws": "lightfog"
   },
   {
     "name": "door",
-    "points": 15,
+    "points": 16,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/doors/stone-door.png",
     "xws": "stonedoor"
   },
   {
     "name": "door",
-    "points": 16,
+    "points": 17,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/doors/wood-door.png",
     "xws": "wooddoor"
   },
   {
     "name": "hot coals",
-    "points": 17,
+    "points": 18,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/hazardous-terrain/hot-coals.png",
     "xws": "hotcoals"
   },
   {
     "name": "thorns",
-    "points": 18,
+    "points": 19,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/hazardous-terrain/thorns.png",
     "xws": "thorns"
   },
   {
     "name": "altar",
-    "points": 19,
+    "points": 20,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/altar.png",
     "xws": "altar"
   },
   {
     "name": "barrel",
-    "points": 20,
+    "points": 21,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/barrel.png",
     "xws": "barrel"
   },
   {
     "name": "bookcase",
-    "points": 21,
+    "points": 22,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/bookcase.png",
     "xws": "bookcase"
   },
   {
     "name": "boulder",
-    "points": 22,
+    "points": 23,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/boulder-1.png",
     "xws": "boulder"
   },
   {
     "name": "boulder",
-    "points": 23,
+    "points": 24,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/boulder-2.png",
     "xws": "boulder"
   },
   {
     "name": "boulder",
-    "points": 24,
+    "points": 25,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/boulder-3.png",
     "xws": "boulder"
   },
   {
     "name": "bush",
-    "points": 25,
+    "points": 26,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/bush.png",
     "xws": "bush"
   },
   {
     "name": "cabinet",
-    "points": 26,
+    "points": 27,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/cabinet.png",
     "xws": "cabinet"
   },
   {
     "name": "crate",
-    "points": 27,
+    "points": 28,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/crate.png",
     "xws": "crate"
   },
   {
     "name": "crystal",
-    "points": 28,
+    "points": 29,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/crystal.png",
     "xws": "crystal"
   },
   {
     "name": "dark pit",
-    "points": 29,
+    "points": 30,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/dark-pit.png",
     "xws": "darkpit"
   },
   {
     "name": "fountain",
-    "points": 30,
+    "points": 31,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/fountain.png",
     "xws": "fountain"
   },
   {
     "name": "nest",
-    "points": 31,
+    "points": 32,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/nest.png",
     "xws": "nest"
   },
   {
     "name": "rock column",
-    "points": 32,
+    "points": 33,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/rock-column.png",
     "xws": "rockcolumn"
   },
   {
     "name": "sarcophagus",
-    "points": 33,
+    "points": 34,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/sarcophagus.png",
     "xws": "sarcophagus"
   },
   {
     "name": "shelf",
-    "points": 34,
+    "points": 35,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/shelf.png",
     "xws": "shelf"
   },
   {
     "name": "stalagmites",
-    "points": 35,
+    "points": 36,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/stalagmites.png",
     "xws": "stalagmites"
   },
   {
     "name": "stone pillar",
-    "points": 36,
+    "points": 37,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/stone-pillar.png",
     "xws": "stonepillar"
   },
   {
     "name": "stump",
-    "points": 37,
+    "points": 38,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/stump.png",
     "xws": "stump"
   },
   {
     "name": "table",
-    "points": 38,
+    "points": 39,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/table.png",
     "xws": "table"
   },
   {
     "name": "totem",
-    "points": 39,
+    "points": 40,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/totem.png",
     "xws": "totem"
   },
   {
     "name": "tree",
-    "points": 40,
+    "points": 41,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/tree.png",
     "xws": "tree"
   },
   {
     "name": "wall section",
-    "points": 41,
+    "points": 42,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/obstacles/wall-section.png",
     "xws": "wallsection"
   },
   {
     "name": "trap",
-    "points": 42,
+    "points": 43,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/traps/bear-trap.png",
     "xws": "beartrap"
   },
   {
     "name": "trap",
-    "points": 43,
+    "points": 44,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/traps/poison-gas-trap.png",
     "xws": "poisongastrap"
   },
   {
     "name": "trap",
-    "points": 44,
+    "points": 45,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/traps/spike-pit-trap.png",
     "xws": "spikepittrap"
   },
   {
     "name": "coin",
-    "points": 45,
+    "points": 46,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/treasures/coin-1.png",
     "xws": "coin1"
   },
   {
     "name": "coin",
-    "points": 46,
+    "points": 47,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/treasures/coin-5.png",
     "xws": "coin5"
   },
   {
     "name": "treasure",
-    "points": 47,
+    "points": 48,
+    "expansion": "Gloomhaven",
     "image": "overlay-tokens/treasures/treasure.png",
     "xws": "treasure"
   }
