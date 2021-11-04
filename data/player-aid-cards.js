d53e73a7d07e08dcf8ee7154d1310456669453eb
any2cards <any2cards@yahoo.com>
2021-11-04 15:11:49 -0700

    Implement Expansions Value

--- data/player-aid-cards.js
+++ data/player-aid-cards.js
@@ -2,48 +2,56 @@
   {
     "name": "halt bottom front",
     "points": 0,
+    "expansion": "Forgotten Circles",
     "image": "character-ability-cards-revised/ZZ/halt-bottom-front.png",
     "xws": "haltbottomfront"
   },
   {
     "name": "halt bottom back",
     "points": 1,
+    "expansion": "Forgotten Circles",
     "image": "character-ability-cards-revised/ZZ/halt-bottom-back.png",
     "xws": "haltbottomback"
   },
   {
     "name": "halt top front",
     "points": 2,
+    "expansion": "Forgotten Circles",
     "image": "character-ability-cards-revised/ZZ/halt-top-front.png",
     "xws": "halttopfront"
   },
   {
     "name": "halt top back",
     "points": 3,
+    "expansion": "Forgotten Circles",
     "image": "character-ability-cards-revised/ZZ/halt-top-back.png",
     "xws": "halttopback"
   },
   {
     "name": "player reference back",
     "points": 4,
+    "expansion": "Gloomhaven",
     "image": "player-aid-cards/gh-player-reference-back.png",
     "xws": "playerreferenceback"
   },
   {
     "name": "player reference front",
     "points": 5,
+    "expansion": "Gloomhaven",
     "image": "player-aid-cards/gh-player-reference-front.png",
     "xws": "playerreferencefront"
   },
   {
     "name": "player reference back",
     "points": 6,
+    "expansion": "Forgotten Circles",
     "image": "player-aid-cards/fc-player-reference-back.png",
     "xws": "playerreferenceback"
   },
   {
     "name": "player reference front",
     "points": 7,
+    "expansion": "Forgotten Circles",
     "image": "player-aid-cards/fc-player-reference-front.png",
     "xws": "playerreferencefront"
   }
