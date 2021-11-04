bb246780c6b05c0e210775a7d17f616272ee2816
any2cards <any2cards@yahoo.com>
2021-11-04 13:50:11 -0700

    Implement Expansions Value

--- data/map-tiles.js
+++ data/map-tiles.js
@@ -2,360 +2,420 @@
   {
     "name": "a1a",
     "points": 0,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a1a.png",
     "xws": "a1a"
   },
   {
     "name": "a1b",
     "points": 1,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a1b.png",
     "xws": "a1b"
   },
   {
     "name": "a2a",
     "points": 2,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a2a.png",
     "xws": "a2a"
   },
   {
     "name": "a2b",
     "points": 3,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a2b.png",
     "xws": "a2b"
   },
   {
     "name": "a3a",
     "points": 4,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a3a.png",
     "xws": "a3a"
   },
   {
     "name": "a3b",
     "points": 5,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a3b.png",
     "xws": "a3b"
   },
   {
     "name": "a4a",
     "points": 6,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a4a.png",
     "xws": "a4a"
   },
   {
     "name": "a4b",
     "points": 7,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/a4b.png",
     "xws": "a4b"
   },
   {
     "name": "b1a",
     "points": 8,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b1a.png",
     "xws": "b1a"
   },
   {
     "name": "b1b",
     "points": 9,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b1b.png",
     "xws": "b1b"
   },
   {
     "name": "b2a",
     "points": 10,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b2a.png",
     "xws": "b2a"
   },
   {
     "name": "b2b",
     "points": 11,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b2b.png",
     "xws": "b2b"
   },
   {
     "name": "b3a",
     "points": 12,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b3a.png",
     "xws": "b3a"
   },
   {
     "name": "b3b",
     "points": 13,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b3b.png",
     "xws": "b3b"
   },
   {
     "name": "b4a",
     "points": 14,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b4a.png",
     "xws": "b4a"
   },
   {
     "name": "b4b",
     "points": 15,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/b4b.png",
     "xws": "b4b"
   },
   {
     "name": "c1a",
     "points": 16,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/c1a.png",
     "xws": "c1a"
   },
   {
     "name": "c1b",
     "points": 17,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/c1b.png",
     "xws": "c1b"
   },
   {
     "name": "c2a",
     "points": 18,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/c2a.png",
     "xws": "c2a"
   },
   {
     "name": "c2b",
     "points": 19,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/c2b.png",
     "xws": "c2b"
   },
   {
     "name": "d1a",
     "points": 20,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/d1a.png",
     "xws": "d1a"
   },
   {
     "name": "d1b",
     "points": 21,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/d1b.png",
     "xws": "d1b"
   },
   {
     "name": "d2a",
     "points": 22,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/d2a.png",
     "xws": "d2a"
   },
   {
     "name": "d2b",
     "points": 23,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/d2b.png",
     "xws": "d2b"
   },
   {
     "name": "e1a",
     "points": 24,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/e1a.png",
     "xws": "e1a"
   },
   {
     "name": "e1b",
     "points": 25,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/e1b.png",
     "xws": "e1b"
   },
   {
     "name": "f1a",
     "points": 26,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/f1a.png",
     "xws": "f1a"
   },
   {
     "name": "f1b",
     "points": 27,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/f1b.png",
     "xws": "f1b"
   },
   {
     "name": "g1a",
     "points": 28,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/g1a.png",
     "xws": "g1a"
   },
   {
     "name": "g1b",
     "points": 29,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/g1b.png",
     "xws": "g1b"
   },
   {
     "name": "g2a",
     "points": 30,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/g2a.png",
     "xws": "g2a"
   },
   {
     "name": "g2b",
     "points": 31,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/g2b.png",
     "xws": "g2b"
   },
   {
     "name": "h1a",
     "points": 32,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/h1a.png",
     "xws": "h1a"
   },
   {
     "name": "h1b",
     "points": 33,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/h1b.png",
     "xws": "h1b"
   },
   {
     "name": "h2a",
     "points": 34,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/h2a.png",
     "xws": "h2a"
   },
   {
     "name": "h2b",
     "points": 35,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/h2b.png",
     "xws": "h2b"
   },
   {
     "name": "h3a",
     "points": 36,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/h3a.png",
     "xws": "h3a"
   },
   {
     "name": "h3b",
     "points": 37,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/h3b.png",
     "xws": "h3b"
   },
   {
     "name": "i1a",
     "points": 38,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/i1a.png",
     "xws": "i1a"
   },
   {
     "name": "i1b",
     "points": 39,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/i1b.png",
     "xws": "i1b"
   },
   {
     "name": "i2a",
     "points": 40,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/i2a.png",
     "xws": "i2a"
   },
   {
     "name": "i2b",
     "points": 41,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/i2b.png",
     "xws": "i2b"
   },
   {
     "name": "j1a",
     "points": 42,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/j1a.png",
     "xws": "j1a"
   },
   {
     "name": "j1b",
     "points": 43,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/j1b.png",
     "xws": "j1b"
   },
   {
     "name": "j2a",
     "points": 44,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/j2a.png",
     "xws": "j2a"
   },
   {
     "name": "j2b",
     "points": 45,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/j2b.png",
     "xws": "j2b"
   },
   {
     "name": "k1a",
     "points": 46,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/k1a.png",
     "xws": "k1a"
   },
   {
     "name": "k1b",
     "points": 47,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/k1b.png",
     "xws": "k1b"
   },
   {
     "name": "k2a",
     "points": 48,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/k2a.png",
     "xws": "k2a"
   },
   {
     "name": "k2b",
     "points": 49,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/k2b.png",
     "xws": "k2b"
   },
   {
     "name": "l1a",
     "points": 50,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/l1a.png",
     "xws": "l1a"
   },
   {
     "name": "l1b",
     "points": 51,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/l1b.png",
     "xws": "l1b"
   },
   {
     "name": "l2a",
     "points": 52,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/l2a.png",
     "xws": "l2a"
   },
   {
     "name": "l2b",
     "points": 53,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/l2b.png",
     "xws": "l2b"
   },
   {
     "name": "l3a",
     "points": 54,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/l3a.png",
     "xws": "l3a"
   },
   {
     "name": "l3b",
     "points": 55,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/l3b.png",
     "xws": "l3b"
   },
   {
     "name": "m1a",
     "points": 56,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/m1a.png",
     "xws": "m1a"
   },
   {
     "name": "m1b",
     "points": 57,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/m1b.png",
     "xws": "m1b"
   },
   {
     "name": "n1a",
     "points": 58,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/n1a.png",
     "xws": "n1a"
   },
   {
     "name": "n1b",
     "points": 59,
+    "expansion": "Gloomhaven",
     "image": "map-tiles/n1b.png",
     "xws": "n1b"
   }
