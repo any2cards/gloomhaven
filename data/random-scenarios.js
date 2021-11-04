762c8ef0bc4f6df2eb88e4ea07ce9689db066b46
any2cards <any2cards@yahoo.com>
2021-11-04 16:22:30 -0700

    Implement Expansions Value

--- data/random-scenarios.js
+++ data/random-scenarios.js
@@ -1,116 +1,135 @@
 [
   {
-    "name": "arcane library",
+    "name": "random scenario 63",
     "points": 0,
-    "image": "random-scenarios/arcane-library.png",
-    "xws": "arcanelibrary"
-  },
-  {
-    "name": "chained isle",
-    "points": 1,
-    "image": "random-scenarios/chained-isle.png",
-    "xws": "chainedisle"
-  },
-  {
-    "name": "clockwork cove",
-    "points": 2,
-    "image": "random-scenarios/clockwork-cove.png",
-    "xws": "clockworkcove"
-  },
-  {
-    "name": "magma pit",
-    "points": 3,
+    "expansion": "Gloomhaven",
     "image": "random-scenarios/magma-pit.png",
     "xws": "magmapit"
   },
   {
-    "name": "random scenario back",
-    "points": 4,
-    "image": "random-scenarios/random-scenario-back.png",
-    "xws": "randomscenarioback"
+    "name": "magma pit",
+    "points": 1,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/magma-pit.png",
+    "xws": "magmapit"
   },
   {
-    "name": "sulfur mine",
-    "points": 5,
-    "image": "random-scenarios/sulfur-mine.png",
-    "xws": "sulfurmine"
-  },
-  {
-    "name": "toxic moor",
-    "points": 6,
-    "image": "random-scenarios/toxic-moor.png",
-    "xws": "toxicmoor"
-  },
-  {
-    "name": "underwater lagoon",
-    "points": 7,
+    "name": "random scenario 64",
+    "points": 2,
+    "expansion": "Gloomhaven",
     "image": "random-scenarios/underwater-lagoon.png",
     "xws": "underwaterlagoon"
   },
   {
-    "name": "well of the unfortunate",
-    "points": 8,
-    "image": "random-scenarios/well-of-the-unfortunate.png",
-    "xws": "welloftheunfortunate"
-  },
-  {
-    "name": "windswept highlands",
-    "points": 9,
-    "image": "random-scenarios/windswept-highlands.png",
-    "xws": "windswepthighlands"
-  },
-  {
-    "name": "random scenario 67",
-    "points": 10,
-    "image": "random-scenarios/arcane-library.png",
-    "xws": "arcanelibrary"
-  },
-  {
-    "name": "random scenario 70",
-    "points": 11,
-    "image": "random-scenarios/chained-isle.png",
-    "xws": "chainedisle"
-  },
-  {
-    "name": "random scenario 66",
-    "points": 12,
-    "image": "random-scenarios/clockwork-cove.png",
-    "xws": "clockworkcove"
-  },
-  {
-    "name": "random scenario 63",
-    "points": 13,
-    "image": "random-scenarios/magma-pit.png",
-    "xws": "magmapit"
+    "name": "underwater lagoon",
+    "points": 3,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/underwater-lagoon.png",
+    "xws": "underwaterlagoon"
   },
   {
     "name": "random scenario 65",
-    "points": 14,
+    "points": 4,
+    "expansion": "Gloomhaven",
     "image": "random-scenarios/sulfur-mine.png",
     "xws": "sulfermine"
   },
   {
+    "name": "sulfur mine",
+    "points": 5,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/sulfur-mine.png",
+    "xws": "sulfurmine"
+  },
+  {
+    "name": "random scenario 66",
+    "points": 6,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/clockwork-cove.png",
+    "xws": "clockworkcove"
+  },
+  {
+    "name": "clockwork cove",
+    "points": 7,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/clockwork-cove.png",
+    "xws": "clockworkcove"
+  },
+  {
+    "name": "random scenario 67",
+    "points": 8,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/arcane-library.png",
+    "xws": "arcanelibrary"
+  },
+  {
+    "name": "arcane library",
+    "points": 9,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/arcane-library.png",
+    "xws": "arcanelibrary"
+  },
+  {
     "name": "random scenario 68",
-    "points": 15,
+    "points": 10,
+    "expansion": "Gloomhaven",
     "image": "random-scenarios/toxic-moor.png",
     "xws": "toxicmoor"
   },
   {
-    "name": "random scenario 64",
-    "points": 16,
-    "image": "random-scenarios/underwater-lagoon.png",
-    "xws": "underwaterlagoon"
+    "name": "toxic moor",
+    "points": 11,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/toxic-moor.png",
+    "xws": "toxicmoor"
   },
   {
     "name": "random scenario 69",
-    "points": 17,
+    "points": 12,
+    "expansion": "Gloomhaven",
     "image": "random-scenarios/well-of-the-unfortunate.png",
     "xws": "welloftheunfortunate"
   },
   {
+    "name": "well of the unfortunate",
+    "points": 13,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/well-of-the-unfortunate.png",
+    "xws": "welloftheunfortunate"
+  },
+  {
+    "name": "random scenario 70",
+    "points": 14,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/chained-isle.png",
+    "xws": "chainedisle"
+  },
+  {
+    "name": "chained isle",
+    "points": 15,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/chained-isle.png",
+    "xws": "chainedisle"
+  },
+  {
     "name": "random scenario 71",
-    "points": 18,
+    "points": 16,
+    "expansion": "Gloomhaven",
     "image": "random-scenarios/windswept-highlands.png",
     "xws": "windswepthighlands"
+  },
+  {
+    "name": "windswept highlands",
+    "points": 17,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/windswept-highlands.png",
+    "xws": "windswepthighlands"
+  },
+  {
+    "name": "random scenario back",
+    "points": 18,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/random-scenario-back.png",
+    "xws": "randomscenarioback"
   }
 ]
