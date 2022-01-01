3eb1d8c1094e7f2c266a5a610364ec67ab7fc1ba
any2cards <any2cards@yahoo.com>
2022-01-01 12:21:49 -0800

    Implement STD Events

--- data/events-revised.js
+++ data/events-revised.js
@@ -1,436 +1,436 @@
 [
   {
-    "name": "ce-82-b",
+    "name": "fc-ce-82-b",
     "points": 0,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-82-b.png",
-    "xws": "ce82b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-82-b.png",
+    "xws": "fcce82b"
   },
   {
-    "name": "ce-82-f",
+    "name": "fc-ce-82-f",
     "points": 1,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-82-f.png",
-    "xws": "ce82f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-82-f.png",
+    "xws": "fcce82f"
   },
   {
-    "name": "ce-83-b",
+    "name": "fc-ce-83-b",
     "points": 2,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-83-b.png",
-    "xws": "ce83b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-83-b.png",
+    "xws": "fcce83b"
   },
   {
-    "name": "ce-83-f",
+    "name": "fc-ce-83-f",
     "points": 3,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-83-f.png",
-    "xws": "ce83f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-83-f.png",
+    "xws": "fcce83f"
   },
   {
-    "name": "ce-84-b",
+    "name": "fc-ce-84-b",
     "points": 4,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-84-b.png",
-    "xws": "ce84b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-84-b.png",
+    "xws": "fcce84b"
   },
   {
-    "name": "ce-84-f",
+    "name": "fc-ce-84-f",
     "points": 5,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-84-f.png",
-    "xws": "ce84f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-84-f.png",
+    "xws": "fcce84f"
   },
   {
-    "name": "ce-85-b",
+    "name": "fc-ce-85-b",
     "points": 6,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-85-b.png",
-    "xws": "ce85b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-85-b.png",
+    "xws": "fcce85b"
   },
   {
-    "name": "ce-85-f",
+    "name": "fc-ce-85-f",
     "points": 7,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-85-f.png",
-    "xws": "ce85f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-85-f.png",
+    "xws": "fcce85f"
   },
   {
-    "name": "ce-86-b",
+    "name": "fc-ce-86-b",
     "points": 8,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-86-b.png",
-    "xws": "ce86b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-86-b.png",
+    "xws": "fcce86b"
   },
   {
-    "name": "ce-86-f",
+    "name": "fc-ce-86-f",
     "points": 9,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-86-f.png",
-    "xws": "ce86f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-86-f.png",
+    "xws": "fcce86f"
   },
   {
-    "name": "ce-87-b",
+    "name": "fc-ce-87-b",
     "points": 10,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-87-b.png",
-    "xws": "ce87b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-87-b.png",
+    "xws": "fcce87b"
   },
   {
-    "name": "ce-87-f",
+    "name": "fc-ce-87-f",
     "points": 11,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-87-f.png",
-    "xws": "ce87f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-87-f.png",
+    "xws": "fcce87f"
   },
   {
-    "name": "ce-88-b",
+    "name": "fc-ce-88-b",
     "points": 12,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-88-b.png",
-    "xws": "ce88b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-88-b.png",
+    "xws": "fcce88b"
   },
   {
-    "name": "ce-88-f",
+    "name": "fc-ce-88-f",
     "points": 13,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-88-f.png",
-    "xws": "ce88f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-88-f.png",
+    "xws": "fcce88f"
   },
   {
-    "name": "ce-89-b",
+    "name": "fc-ce-89-b",
     "points": 14,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-89-b.png",
-    "xws": "ce89b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-89-b.png",
+    "xws": "fcce89b"
   },
   {
-    "name": "ce-89-f",
+    "name": "fc-ce-89-f",
     "points": 15,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-89-f.png",
-    "xws": "ce89f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-89-f.png",
+    "xws": "fcce89f"
   },
   {
-    "name": "ce-90-b",
+    "name": "fc-ce-90-b",
     "points": 16,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-90-b.png",
-    "xws": "ce90b"
+    "image": "events-revised/forgotten-circles/city/fc-ce-90-b.png",
+    "xws": "fcce90b"
   },
   {
-    "name": "ce-90-f",
+    "name": "fc-ce-90-f",
     "points": 17,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/city/ce-90-f.png",
-    "xws": "ce90f"
+    "image": "events-revised/forgotten-circles/city/fc-ce-90-f.png",
+    "xws": "fcce90f"
   },
   {
-    "name": "rf-01-f",
+    "name": "fc-rf-01-f",
     "points": 18,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-01-f.png",
-    "xws": "rf01f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-01-f.png",
+    "xws": "fcrf01f"
   },
   {
-    "name": "rf-01-b",
+    "name": "fc-rf-01-b",
     "points": 19,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-01-b.png",
-    "xws": "rf01b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-01-b.png",
+    "xws": "fcrf01b"
   },
   {
-    "name": "rf-02-b",
+    "name": "fc-rf-02-b",
     "points": 20,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-02-b.png",
-    "xws": "rf02b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-02-b.png",
+    "xws": "fcrf02b"
   },
   {
-    "name": "rf-02-f",
+    "name": "fc-rf-02-f",
     "points": 21,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-02-f.png",
-    "xws": "rf02f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-02-f.png",
+    "xws": "fcrf02f"
   },
   {
-    "name": "rf-03-b",
+    "name": "fc-rf-03-b",
     "points": 22,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-03-b.png",
-    "xws": "rf03b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-03-b.png",
+    "xws": "fcrf03b"
   },
   {
-    "name": "rf-03-f",
+    "name": "fc-rf-03-f",
     "points": 23,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-03-f.png",
-    "xws": "rf03f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-03-f.png",
+    "xws": "fcrf03f"
   },
   {
-    "name": "rf-04-b",
+    "name": "fc-rf-04-b",
     "points": 24,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-04-b.png",
-    "xws": "rf04b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-04-b.png",
+    "xws": "fcrf04b"
   },
   {
-    "name": "rf-04-f",
+    "name": "fc-rf-04-f",
     "points": 25,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-04-f.png",
-    "xws": "rf04f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-04-f.png",
+    "xws": "fcrf04f"
   },
   {
-    "name": "rf-05-b",
+    "name": "fc-rf-05-b",
     "points": 26,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-05-b.png",
-    "xws": "rf05b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-05-b.png",
+    "xws": "fcrf05b"
   },
   {
-    "name": "rf-05-f",
+    "name": "fc-rf-05-f",
     "points": 27,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-05-f.png",
-    "xws": "rf05f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-05-f.png",
+    "xws": "fcrf05f"
   },
   {
-    "name": "rf-06-b",
+    "name": "fc-rf-06-b",
     "points": 28,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-06-b.png",
-    "xws": "rf06b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-06-b.png",
+    "xws": "fcrf06b"
   },
   {
-    "name": "rf-06-f",
+    "name": "fc-rf-06-f",
     "points": 29,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-06-f.png",
-    "xws": "rf06f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-06-f.png",
+    "xws": "fcrf06f"
   },
   {
-    "name": "rf-07-b",
+    "name": "fc-rf-07-b",
     "points": 30,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-07-b.png",
-    "xws": "rf07b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-07-b.png",
+    "xws": "fcrf07b"
   },
   {
-    "name": "rf-07-f",
+    "name": "fc-rf-07-f",
     "points": 31,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-07-f.png",
-    "xws": "rf07f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-07-f.png",
+    "xws": "fcrf07f"
   },
   {
-    "name": "rf-08-b",
+    "name": "fc-rf-08-b",
     "points": 32,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-08-b.png",
-    "xws": "rf08b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-08-b.png",
+    "xws": "fcrf08b"
   },
   {
-    "name": "rf-08-f",
+    "name": "fc-rf-08-f",
     "points": 33,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-08-f.png",
-    "xws": "rf08f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-08-f.png",
+    "xws": "fcrf08f"
   },
   {
-    "name": "rf-09-b",
+    "name": "fc-rf-09-b",
     "points": 34,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-09-b.png",
-    "xws": "rf09b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-09-b.png",
+    "xws": "fcrf09b"
   },
   {
-    "name": "rf-09-f",
+    "name": "fc-rf-09-f",
     "points": 35,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-09-f.png",
-    "xws": "rf09f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-09-f.png",
+    "xws": "fcrf09f"
   },
   {
-    "name": "rf-10-b",
+    "name": "fc-rf-10-b",
     "points": 36,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-10-b.png",
-    "xws": "rf10b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-10-b.png",
+    "xws": "fcrf10b"
   },
   {
-    "name": "rf-10-f",
+    "name": "fc-rf-10-f",
     "points": 37,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-10-f.png",
-    "xws": "rf10f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-10-f.png",
+    "xws": "fcrf10f"
   },
   {
-    "name": "rf-11-b",
+    "name": "fc-rf-11-b",
     "points": 38,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-11-b.png",
-    "xws": "rf11b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-11-b.png",
+    "xws": "fcrf11b"
   },
   {
-    "name": "rf-11-f",
+    "name": "fc-rf-11-f",
     "points": 39,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-11-f.png",
-    "xws": "rf11f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-11-f.png",
+    "xws": "fcrf11f"
   },
   {
-    "name": "rf-12-b",
+    "name": "fc-rf-12-b",
     "points": 40,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-12-b.png",
-    "xws": "rf12b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-12-b.png",
+    "xws": "fcrf12b"
   },
   {
-    "name": "rf-12-f",
+    "name": "fc-rf-12-f",
     "points": 41,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-12-f.png",
-    "xws": "rf12f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-12-f.png",
+    "xws": "fcrf12f"
   },
   {
-    "name": "rf-13-b",
+    "name": "fc-rf-13-b",
     "points": 42,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-13-b.png",
-    "xws": "rf13b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-13-b.png",
+    "xws": "fcrf13b"
   },
   {
-    "name": "rf-13-f",
+    "name": "fc-rf-13-f",
     "points": 43,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-13-f.png",
-    "xws": "rf13f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-13-f.png",
+    "xws": "fcrf13f"
   },
   {
-    "name": "rf-14-b",
+    "name": "fc-rf-14-b",
     "points": 44,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-14-b.png",
-    "xws": "rf14b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-14-b.png",
+    "xws": "fcrf14b"
   },
   {
-    "name": "rf-14-f",
+    "name": "fc-rf-14-f",
     "points": 45,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-14-f.png",
-    "xws": "rf14f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-14-f.png",
+    "xws": "fcrf14f"
   },
   {
-    "name": "rf-15-b",
+    "name": "fc-rf-15-b",
     "points": 46,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-15-b.png",
-    "xws": "rf15b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-15-b.png",
+    "xws": "fcrf15b"
   },
   {
-    "name": "rf-15-f",
+    "name": "fc-rf-15-f",
     "points": 47,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-15-f.png",
-    "xws": "rf15f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-15-f.png",
+    "xws": "fcrf15f"
   },
   {
-    "name": "rf-16-b",
+    "name": "fc-rf-16-b",
     "points": 48,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-16-b.png",
-    "xws": "rf16b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-16-b.png",
+    "xws": "fcrf16b"
   },
   {
-    "name": "rf-16-f",
+    "name": "fc-rf-16-f",
     "points": 49,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-16-f.png",
-    "xws": "rf16f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-16-f.png",
+    "xws": "fcrf16f"
   },
   {
-    "name": "rf-17-b",
+    "name": "fc-rf-17-b",
     "points": 50,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-17-b.png",
-    "xws": "rf17b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-17-b.png",
+    "xws": "fcrf17b"
   },
   {
-    "name": "rf-17-f",
+    "name": "fc-rf-17-f",
     "points": 51,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-17-f.png",
-    "xws": "rf17f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-17-f.png",
+    "xws": "fcrf17f"
   },
   {
-    "name": "rf-18-b",
+    "name": "fc-rf-18-b",
     "points": 52,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-18-b.png",
-    "xws": "rf18b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-18-b.png",
+    "xws": "fcrf18b"
   },
   {
-    "name": "rf-18-f",
+    "name": "fc-rf-18-f",
     "points": 53,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-18-f.png",
-    "xws": "rf18f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-18-f.png",
+    "xws": "fcrf18f"
   },
   {
-    "name": "rf-19-b",
+    "name": "fc-rf-19-b",
     "points": 54,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-19-b.png",
-    "xws": "rf19b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-19-b.png",
+    "xws": "fcrf19b"
   },
   {
-    "name": "rf-19-f",
+    "name": "fc-rf-19-f",
     "points": 55,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-19-f.png",
-    "xws": "rf19f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-19-f.png",
+    "xws": "fcrf19f"
   },
   {
-    "name": "rf-20-b",
+    "name": "fc-rf-20-b",
     "points": 56,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-20-b.png",
-    "xws": "rf20b"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-20-b.png",
+    "xws": "fcrf20b"
   },
   {
-    "name": "rf-20-f",
+    "name": "fc-rf-20-f",
     "points": 57,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/rift/rf-20-f.png",
-    "xws": "rf20f"
+    "image": "events-revised/forgotten-circles/rift/fc-rf-20-f.png",
+    "xws": "fcrf20f"
   },
   {
-    "name": "re-82-b",
+    "name": "fc-re-82-b",
     "points": 58,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/road/re-82-b.png",
-    "xws": "re82b"
+    "image": "events-revised/forgotten-circles/road/fc-re-82-b.png",
+    "xws": "fcre82b"
   },
   {
-    "name": "re-82-f",
+    "name": "fc-re-82-f",
     "points": 59,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/road/re-82-f.png",
-    "xws": "re82f"
+    "image": "events-revised/forgotten-circles/road/fc-re-82-f.png",
+    "xws": "fcre82f"
   },
   {
-    "name": "re-83-b",
+    "name": "fc-re-83-b",
     "points": 60,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/road/re-83-b.png",
-    "xws": "re83b"
+    "image": "events-revised/forgotten-circles/road/fc-re-83-b.png",
+    "xws": "fcre83b"
   },
   {
-    "name": "re-83-f",
+    "name": "fc-re-83-f",
     "points": 61,
     "expansion": "Forgotten Circles",
-    "image": "events-revised/road/re-83-f.png",
-    "xws": "re83f"
+    "image": "events-revised/forgotten-circles/road/fc-re-83-f.png",
+    "xws": "fcre83f"
   }
 ]
