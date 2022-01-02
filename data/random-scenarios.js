e17a8eeef319266c0e22ced1f8ce3261d3f3a756
any2cards <any2cards@yahoo.com>
2022-01-02 07:53:50 -0800

    Implement STD RS

--- data/random-scenarios.js
+++ data/random-scenarios.js
@@ -1,135 +1,135 @@
 [
   {
-    "name": "random scenario 63",
+    "name": "random scenario back",
     "points": 0,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/magma-pit.png",
+    "image": "random-scenarios/gloomhaven/gh-random-scenario-back.png",
+    "xws": "randomscenarioback"
+  },
+  {
+    "name": "random scenario 63",
+    "points": 1,
+    "expansion": "Gloomhaven",
+    "image": "random-scenarios/gloomhaven/gh-magma-pit.png",
     "xws": "magmapit"
   },
   {
     "name": "magma pit",
-    "points": 1,
+    "points": 2,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/magma-pit.png",
+    "image": "random-scenarios/gloomhaven/gh-magma-pit.png",
     "xws": "magmapit"
   },
   {
     "name": "random scenario 64",
-    "points": 2,
+    "points": 3,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/underwater-lagoon.png",
+    "image": "random-scenarios/gloomhaven/gh-underwater-lagoon.png",
     "xws": "underwaterlagoon"
   },
   {
     "name": "underwater lagoon",
-    "points": 3,
+    "points": 4,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/underwater-lagoon.png",
+    "image": "random-scenarios/gloomhaven/gh-underwater-lagoon.png",
     "xws": "underwaterlagoon"
   },
   {
     "name": "random scenario 65",
-    "points": 4,
+    "points": 5,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/sulfur-mine.png",
+    "image": "random-scenarios/gloomhaven/gh-sulfur-mine.png",
     "xws": "sulfermine"
   },
   {
     "name": "sulfur mine",
-    "points": 5,
+    "points": 6,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/sulfur-mine.png",
+    "image": "random-scenarios/gloomhaven/gh-sulfur-mine.png",
     "xws": "sulfurmine"
   },
   {
     "name": "random scenario 66",
-    "points": 6,
+    "points": 7,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/clockwork-cove.png",
+    "image": "random-scenarios/gloomhaven/gh-clockwork-cove.png",
     "xws": "clockworkcove"
   },
   {
     "name": "clockwork cove",
-    "points": 7,
+    "points": 8,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/clockwork-cove.png",
+    "image": "random-scenarios/gloomhaven/gh-clockwork-cove.png",
     "xws": "clockworkcove"
   },
   {
     "name": "random scenario 67",
-    "points": 8,
+    "points": 9,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/arcane-library.png",
+    "image": "random-scenarios/gloomhaven/gh-arcane-library.png",
     "xws": "arcanelibrary"
   },
   {
     "name": "arcane library",
-    "points": 9,
+    "points": 10,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/arcane-library.png",
+    "image": "random-scenarios/gloomhaven/gh-arcane-library.png",
     "xws": "arcanelibrary"
   },
   {
     "name": "random scenario 68",
-    "points": 10,
+    "points": 11,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/toxic-moor.png",
+    "image": "random-scenarios/gloomhaven/gh-toxic-moor.png",
     "xws": "toxicmoor"
   },
   {
     "name": "toxic moor",
-    "points": 11,
+    "points": 12,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/toxic-moor.png",
+    "image": "random-scenarios/gloomhaven/gh-toxic-moor.png",
     "xws": "toxicmoor"
   },
   {
     "name": "random scenario 69",
-    "points": 12,
+    "points": 13,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/well-of-the-unfortunate.png",
+    "image": "random-scenarios/gloomhaven/gh-well-of-the-unfortunate.png",
     "xws": "welloftheunfortunate"
   },
   {
     "name": "well of the unfortunate",
-    "points": 13,
+    "points": 14,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/well-of-the-unfortunate.png",
+    "image": "random-scenarios/gloomhaven/gh-well-of-the-unfortunate.png",
     "xws": "welloftheunfortunate"
   },
   {
     "name": "random scenario 70",
-    "points": 14,
+    "points": 15,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/chained-isle.png",
+    "image": "random-scenarios/gloomhaven/gh-chained-isle.png",
     "xws": "chainedisle"
   },
   {
     "name": "chained isle",
-    "points": 15,
+    "points": 16,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/chained-isle.png",
+    "image": "random-scenarios/gloomhaven/gh-chained-isle.png",
     "xws": "chainedisle"
   },
   {
     "name": "random scenario 71",
-    "points": 16,
+    "points": 17,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/windswept-highlands.png",
+    "image": "random-scenarios/gloomhaven/gh-windswept-highlands.png",
     "xws": "windswepthighlands"
   },
   {
     "name": "windswept highlands",
-    "points": 17,
-    "expansion": "Gloomhaven",
-    "image": "random-scenarios/windswept-highlands.png",
-    "xws": "windswepthighlands"
-  },
-  {
-    "name": "random scenario back",
     "points": 18,
     "expansion": "Gloomhaven",
-    "image": "random-scenarios/random-scenario-back.png",
-    "xws": "randomscenarioback"
+    "image": "random-scenarios/gloomhaven/gh-windswept-highlands.png",
+    "xws": "windswepthighlands"
   }
 ]
