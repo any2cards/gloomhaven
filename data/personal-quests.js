2b309e7258480a9902d84d4b4b8f83639042b4dd
any2cards <any2cards@yahoo.com>
2022-01-02 07:21:31 -0800

    Implement STD PQ

--- data/personal-quests.js
+++ data/personal-quests.js
@@ -1,345 +1,345 @@
 [
   {
-    "name": "pq-510",
+    "name": "gh-pq-back",
     "points": 0,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-510.png",
+    "image": "personal-quests/gloomhaven/gh-pq-back.png",
+    "xws": "pqback"
+  },  
+  {
+    "name": "gh-pq-510",
+    "points": 1,
+    "expansion": "Gloomhaven",
+    "image": "personal-quests/gloomhaven/gh-pq-510.png",
     "xws": "pq510"
   },
   {
     "name": "seeker of xorn",
-    "points": 25,
+    "points": 2,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-510.png",
+    "image": "personal-quests/gloomhaven/gh-pq-510.png",
     "xws": "pq510"
   },
   {
-    "name": "pq-511",
-    "points": 1,
+    "name": "gh-pq-511",
+    "points": 3,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-511.png",
+    "image": "personal-quests/gloomhaven/gh-pq-511.png",
     "xws": "pq511"
   },
   {
     "name": "merchant class",
-    "points": 26,
+    "points": 4,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-511.png",
+    "image": "personal-quests/gloomhaven/gh-pq-511.png",
     "xws": "pq511"
   },
   {
-    "name": "pq-512",
-    "points": 2,
+    "name": "gh-pq-512",
+    "points": 5,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-512.png",
+    "image": "personal-quests/gloomhaven/gh-pq-512.png",
     "xws": "pq512"
   },
   {
     "name": "greed is good",
-    "points": 27,
+    "points": 6,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-512.png",
+    "image": "personal-quests/gloomhaven/gh-pq-512.png",
     "xws": "pq512"
   },
   {
-    "name": "pq-513",
-    "points": 3,
+    "name": "gh-pq-513",
+    "points": 7,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-513.png",
+    "image": "personal-quests/gloomhaven/gh-pq-513.png",
     "xws": "pq513"
   },
   {
     "name": "finding the cure",
-    "points": 28,
+    "points": 8,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-513.png",
+    "image": "personal-quests/gloomhaven/gh-pq-513.png",
     "xws": "pq513"
   },
   {
-    "name": "pq-514",
-    "points": 4,
+    "name": "gh-pq-514",
+    "points": 9,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-514.png",
+    "image": "personal-quests/gloomhaven/gh-pq-514.png",
     "xws": "pq514"
   },
   {
     "name": "a study of anatomy",
-    "points": 29,
+    "points": 10,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-514.png",
+    "image": "personal-quests/gloomhaven/gh-pq-514.png",
     "xws": "pq514"
   },
   {
-    "name": "pq-515",
-    "points": 5,
+    "name": "gh-pq-515",
+    "points": 11,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-515.png",
+    "image": "personal-quests/gloomhaven/gh-pq-515.png",
     "xws": "pq515"
   },
   {
     "name": "law bringer",
-    "points": 30,
+    "points": 12,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-515.png",
+    "image": "personal-quests/gloomhaven/gh-pq-515.png",
     "xws": "pq515"
   },
   {
-    "name": "pq-516",
-    "points": 6,
+    "name": "gh-pq-516",
+    "points": 13,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-516.png",
+    "image": "personal-quests/gloomhaven/gh-pq-516.png",
     "xws": "pq516"
   },
   {
     "name": "pounds of flesh",
-    "points": 31,
+    "points": 14,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-516.png",
+    "image": "personal-quests/gloomhaven/gh-pq-516.png",
     "xws": "pq516"
   },
   {
-    "name": "pq-517",
-    "points": 7,
+    "name": "gh-pq-517",
+    "points": 15,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-517.png",
+    "image": "personal-quests/gloomhaven/gh-pq-517.png",
     "xws": "pq517"
   },
   {
     "name": "trophy hunt",
-    "points": 32,
+    "points": 16,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-517.png",
+    "image": "personal-quests/gloomhaven/gh-pq-517.png",
     "xws": "pq517"
   },
   {
-    "name": "pq-518",
-    "points": 8,
+    "name": "gh-pq-518",
+    "points": 17,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-518.png",
+    "image": "personal-quests/gloomhaven/gh-pq-518.png",
     "xws": "pq518"
   },
   {
     "name": "eternal wanderer",
-    "points": 33,
+    "points": 18,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-518.png",
+    "image": "personal-quests/gloomhaven/gh-pq-518.png",
     "xws": "pq518"
   },
   {
-    "name": "pq-519",
-    "points": 9,
+    "name": "gh-pq-519",
+    "points": 19,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-519.png",
+    "image": "personal-quests/gloomhaven/gh-pq-519.png",
     "xws": "pq519"
   },
   {
     "name": "battle legend",
-    "points": 34,
+    "points": 20,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-519.png",
+    "image": "personal-quests/gloomhaven/gh-pq-519.png",
     "xws": "pq519"
   },
   {
-    "name": "pq-520",
-    "points": 10,
+    "name": "gh-pq-520",
+    "points": 21,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-520.png",
+    "image": "personal-quests/gloomhaven/gh-pq-520.png",
     "xws": "pq520"
   },
   {
     "name": "implement of light",
-    "points": 35,
+    "points": 22,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-520.png",
+    "image": "personal-quests/gloomhaven/gh-pq-520.png",
     "xws": "pq520"
   },
   {
-    "name": "pq-521",
-    "points": 11,
+    "name": "gh-pq-521",
+    "points": 23,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-521.png",
+    "image": "personal-quests/gloomhaven/gh-pq-521.png",
     "xws": "pq521"
   },
   {
     "name": "take back the trees",
-    "points": 36,
+    "points": 24,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-521.png",
+    "image": "personal-quests/gloomhaven/gh-pq-521.png",
     "xws": "pq521"
   },
   {
-    "name": "pq-522",
-    "points": 12,
+    "name": "gh-pq-522",
+    "points": 25,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-522.png",
+    "image": "personal-quests/gloomhaven/gh-pq-522.png",
     "xws": "pq522"
   },
   {
     "name": "the thin places",
-    "points": 37,
+    "points": 26,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-522.png",
+    "image": "personal-quests/gloomhaven/gh-pq-522.png",
     "xws": "pq522"
   },
   {
-    "name": "pq-523",
-    "points": 13,
+    "name": "gh-pq-523",
+    "points": 27,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-523.png",
+    "image": "personal-quests/gloomhaven/gh-pq-523.png",
     "xws": "pq523"
   },
   {
     "name": "aberrant slayer",
-    "points": 38,
+    "points": 28,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-523.png",
+    "image": "personal-quests/gloomhaven/gh-pq-523.png",
     "xws": "pq523"
   },
   {
-    "name": "pq-524",
-    "points": 14,
+    "name": "gh-pq-524",
+    "points": 29,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-524.png",
+    "image": "personal-quests/gloomhaven/gh-pq-524.png",
     "xws": "pq524"
   },
   {
     "name": "fearless stand",
-    "points": 39,
+    "points": 30,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-524.png",
+    "image": "personal-quests/gloomhaven/gh-pq-524.png",
     "xws": "pq524"
   },
   {
-    "name": "pq-525",
-    "points": 15,
+    "name": "gh-pq-525",
+    "points": 31,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-525.png",
+    "image": "personal-quests/gloomhaven/gh-pq-525.png",
     "xws": "pq525"
   },
   {
     "name": "piety in all things",
-    "points": 40,
+    "points": 32,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-525.png",
+    "image": "personal-quests/gloomhaven/gh-pq-525.png",
     "xws": "pq525"
   },
   {
-    "name": "pq-526",
-    "points": 16,
+    "name": "gh-pq-526",
+    "points": 33,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-526.png",
+    "image": "personal-quests/gloomhaven/gh-pq-526.png",
     "xws": "pq526"
   },
   {
     "name": "vengeance",
-    "points": 41,
+    "points": 34,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-526.png",
+    "image": "personal-quests/gloomhaven/gh-pq-526.png",
     "xws": "pq526"
   },
   {
-    "name": "pq-527",
-    "points": 17,
+    "name": "gh-pq-527",
+    "points": 35,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-527.png",
+    "image": "personal-quests/gloomhaven/gh-pq-527.png",
     "xws": "pq527"
   },
   {
     "name": "zealot of the blood god",
-    "points": 42,
+    "points": 36,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-527.png",
+    "image": "personal-quests/gloomhaven/gh-pq-527.png",
     "xws": "pq527"
   },
   {
-    "name": "pq-528",
-    "points": 18,
+    "name": "gh-pq-528",
+    "points": 37,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-528.png",
+    "image": "personal-quests/gloomhaven/gh-pq-528.png",
     "xws": "pq528"
   },
   {
     "name": "goliath toppler",
-    "points": 43,
+    "points": 38,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-528.png",
+    "image": "personal-quests/gloomhaven/gh-pq-528.png",
     "xws": "pq528"
   },
   {
-    "name": "pq-529",
-    "points": 19,
+    "name": "gh-pq-529",
+    "points": 39,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-529.png",
+    "image": "personal-quests/gloomhaven/gh-pq-529.png",
     "xws": "pq529"
   },
   {
     "name": "the fall of man",
-    "points": 44,
+    "points": 40,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-529.png",
+    "image": "personal-quests/gloomhaven/gh-pq-529.png",
     "xws": "pq529"
   },
   {
-    "name": "pq-530",
-    "points": 20,
+    "name": "gh-pq-530",
+    "points": 41,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-530.png",
+    "image": "personal-quests/gloomhaven/gh-pq-530.png",
     "xws": "pq530"
   },
   {
     "name": "augmented abilities",
-    "points": 45,
+    "points": 42,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-530.png",
+    "image": "personal-quests/gloomhaven/gh-pq-530.png",
     "xws": "pq530"
   },
   {
-    "name": "pq-531",
-    "points": 21,
+    "name": "gh-pq-531",
+    "points": 43,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-531.png",
+    "image": "personal-quests/gloomhaven/gh-pq-531.png",
     "xws": "pq531"
   },
   {
     "name": "elemental samples",
-    "points": 46,
+    "points": 44,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-531.png",
+    "image": "personal-quests/gloomhaven/gh-pq-531.png",
     "xws": "pq531"
   },
   {
-    "name": "pq-532",
-    "points": 22,
+    "name": "gh-pq-532",
+    "points": 45,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-532.png",
+    "image": "personal-quests/gloomhaven/gh-pq-532.png",
     "xws": "pq532"
   },
   {
     "name": "a helping hand",
-    "points": 47,
+    "points": 46,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-532.png",
+    "image": "personal-quests/gloomhaven/gh-pq-532.png",
     "xws": "pq532"
   },
   {
-    "name": "pq-533",
-    "points": 23,
+    "name": "gh-pq-533",
+    "points": 47,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-533.png",
+    "image": "personal-quests/gloomhaven/gh-pq-533.png",
     "xws": "pq533"
   },
   {
     "name": "the perfect poison",
     "points": 48,
     "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-533.png",
+    "image": "personal-quests/gloomhaven/gh-pq-533.png",
     "xws": "pq533"
-  },
-  {
-    "name": "pq-back",
-    "points": 24,
-    "expansion": "Gloomhaven",
-    "image": "personal-quests/pq-back.png",
-    "xws": "pqback"
-  }  
+  }
 ]
