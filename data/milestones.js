0ae018c28a3ccdc218354c4275bb27963e7d8055
any2cards <any2cards@yahoo.com>
2022-02-12 06:28:46 -0800

    Implement CS AO Milestones

--- data/milestones.js
+++ data/milestones.js
@@ -1,154 +1,196 @@
 [
   {
-    "name": "bombard milestones",
+    "name": "amber aegis milestones",
     "points": 0,
+    "expansion": "Crimson Scales",
+    "image": "milestones/crimson-scales/cs-ms-amber-aegis-front.png",
+    "xws": "amberaegismilestone"
+  },
+  {
+    "name": "amber aegis milestones",
+    "points": 1,
+    "expansion": "Crimson Scales",
+    "image": "milestones/crimson-scales/cs-ms-amber-aegis-back.png",
+    "xws": "amberaegismilestone"
+  },
+  {
+    "name": "artificer milestones",
+    "points": 2,
+    "expansion": "Crimson Scales",
+    "image": "milestones/crimson-scales/cs-ms-artificer-front.png",
+    "xws": "artificermilestone"
+  },
+  {
+    "name": "artificer milestones",
+    "points": 3,
+    "expansion": "Crimson Scales",
+    "image": "milestones/crimson-scales/cs-ms-artificer-back.png",
+    "xws": "artificermilestone"
+  },
+  {
+    "name": "bombard milestones",
+    "points": 4,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-bombard-front.png",
     "xws": "bombardmilestone"
   },
   {
     "name": "bombard milestones",
-    "points": 1,
+    "points": 5,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-bombard-back.png",
     "xws": "bombardmilestone"
   },
   {
     "name": "brightspark milestones",
-    "points": 2,
+    "points": 6,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-brightspark-front.png",
     "xws": "brightsparkmilestone"
   },
   {
     "name": "brightspark milestones",
-    "points": 3,
+    "points": 7,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-brightspark-back.png",
     "xws": "brightsparkmilestone"
   },
   {
     "name": "chainguard milestones",
-    "points": 4,
+    "points": 8,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-chainguard-front.png",
     "xws": "chainguardmilestone"
   },
   {
     "name": "chainguard milestones",
-    "points": 5,
+    "points": 9,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-chainguard-back.png",
     "xws": "chainguardmilestone"
   },
   {
     "name": "chieftain milestones",
-    "points": 6,
+    "points": 10,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-chieftain-front.png",
     "xws": "chieftainmilestone"
   },
   {
     "name": "chieftain milestones",
-    "points": 7,
+    "points": 11,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-chieftain-back.png",
     "xws": "chieftainmilestone"
   },
   {
     "name": "fire knight milestones",
-    "points": 8,
+    "points": 12,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-fire-knight-front.png",
     "xws": "fireknightmilestone"
   },
   {
     "name": "fire knight milestones",
-    "points": 9,
+    "points": 13,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-fire-knight-back.png",
     "xws": "fireknightmilestone"
   },
   {
     "name": "hierophant milestones",
-    "points": 10,
+    "points": 14,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-hierophant-front.png",
     "xws": "hierophantmilestone"
   },
   {
     "name": "hierophant milestones",
-    "points": 11,
+    "points": 15,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-hierophant-back.png",
     "xws": "hierophantmilestone"
   },
   {
     "name": "hollowpact milestones",
-    "points": 12,
+    "points": 16,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-hollowpact-front.png",
     "xws": "hollowpactmilestone"
   },
   {
     "name": "hollowpact milestones",
-    "points": 13,
+    "points": 17,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-hollowpact-back.png",
     "xws": "hollowpactmilestone"
   },
   {
     "name": "luminary milestones",
-    "points": 14,
+    "points": 18,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-luminary-front.png",
     "xws": "luminarymilestone"
   },
   {
     "name": "luminary milestones",
-    "points": 15,
+    "points": 19,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-luminary-back.png",
     "xws": "luminarymilestone"
   },
   {
     "name": "mirefoot milestones",
-    "points": 16,
+    "points": 20,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-mirefoot-front.png",
     "xws": "mirefootmilestone"
   },
   {
     "name": "mirefoot milestones",
-    "points": 17,
+    "points": 21,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-mirefoot-back.png",
     "xws": "mirefootmilestone"
   },
   {
+    "name": "ruinmaw milestones",
+    "points": 22,
+    "expansion": "Crimson Scales",
+    "image": "milestones/crimson-scales/cs-ms-ruinmaw-front.png",
+    "xws": "ruinmawmilestone"
+  },
+  {
+    "name": "ruinmaw milestones",
+    "points": 23,
+    "expansion": "Crimson Scales",
+    "image": "milestones/crimson-scales/cs-ms-ruinmaw-back.png",
+    "xws": "ruinmawmilestone"
+  },
+  {
     "name": "spirit caller milestones",
-    "points": 18,
+    "points": 24,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-spirit-caller-front.png",
     "xws": "spiritcallermilestone"
   },
   {
     "name": "spirit caller milestones",
-    "points": 19,
+    "points": 25,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-spirit-caller-back.png",
     "xws": "spiritcallermilestone"
   },
   {
     "name": "starslinger milestones",
-    "points": 20,
+    "points": 26,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-starslinger-front.png",
     "xws": "starslingermilestone"
   },
   {
     "name": "starslinger milestones",
-    "points": 21,
+    "points": 27,
     "expansion": "Crimson Scales",
     "image": "milestones/crimson-scales/cs-ms-starslinger-back.png",
     "xws": "starslingermilestone"
